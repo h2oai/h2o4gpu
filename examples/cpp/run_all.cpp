bfd6c155ff7c65be65e581821b8afa1e9984a417
foges <unpluggd@gmail.com>
2014-08-19 15:57:43 -0700

    .
    Former-commit-id: f1482476c38ee6604ad42b07227a3593afafb5d0

--- examples/cpp/run_all.cpp
+++ examples/cpp/run_all.cpp
@@ -2,36 +2,36 @@
 
 #include "examples.h"
 
-typedef double real_t;
+typedef float real_t;
 
 int main() {
   double t;
-  printf("\nLasso.\n");
-  t = Lasso<real_t>(100, 1000);
-  printf("Solver Time: %e sec\n", t);
+//  printf("\nLasso.\n");
+//  t = Lasso<real_t>(10000, 100000);
+//  printf("Solver Time: %e sec\n", t);
 
-  printf("\nLasso Path.\n");
-  t = LassoPath<real_t>(100, 1000);
-  printf("Solver Time: %e sec\n", t);
+//  printf("\nLasso Path.\n");
+//  t = LassoPath<real_t>(2000, 1000000);
+//  printf("Solver Time: %e sec\n", t);
 
   printf("\nLogistic Regression.\n");
-  t = LogisticRegression<real_t>(1000, 100);
+  t = LogisticRegression<real_t>(1000000, 2000);
   printf("Solver Time: %e sec\n", t);
 
-//   printf("\nLinear Program in Equality Form.\n");
-//   t = LpEq<real_t>(200, 1000);
-//   printf("Solver Time: %e sec\n", t);
-// 
+//  printf("\nLinear Program in Equality Form.\n");
+//  t = LpEq<real_t>(1000000, 2000);
+//  printf("Solver Time: %e sec\n", t);
+
 //   printf("\nLinear Program in Inequality Form.\n");
 //   t = LpIneq<real_t>(1000, 200);
 //   printf("Solver Time: %e sec\n", t);
-// 
+
 //   printf("\nNon-Negative Least Squares.\n");
 //   t = NonNegL2<real_t>(1000, 200);
 //   printf("Solver Time: %e sec\n", t);
-// 
+
 //   printf("\nSupport Vector Machine.\n");
-//   t = Svm<real_t>(1000, 100);
+//   t = Svm<real_t>(1000000, 2000);
 //   printf("Solver Time: %e sec\n", t);
 
   return 0;
