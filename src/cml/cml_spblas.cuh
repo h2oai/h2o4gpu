406efbe3b4518d9c717571831c4836f2ea08a3e4
foges <chris.fougner@gmail.com>
2014-09-04 12:30:28 -0700

    .
    Former-commit-id: dcffa1b9224e753fabb521ef59df90bc9421cd1e

--- src/cml/cml_spblas.cuh
+++ src/cml/cml_spblas.cuh
@@ -5,6 +5,21 @@
 
 namespace cml {
 
+template <typename T, typename I, CGLS_FMT F>
+solve(cusparseHandle_t handle_s, cublasHandle_t handle_b, spmat<T, I, O> A,
+      const T shift, const T tol, const I maxit, bool quiet) {
+
+  solve(handle_s, handle_b, cusparseMatDescr_t descr, const T *val_a, const INT *ptr_a,
+          const INT *ind_a, const T *val_at, const INT *ptr_at,
+          const INT *ind_at, const INT m, const INT n, const INT nnz,
+          const T *b, T *x, const T shift, const T tol, const INT maxit,
+          bool quiet) {
+
+}
+
+
+
+cusparseStatus_t cusparseDcsr2csc(cusparseHandle_t handle, int m, int n, int nnz, const double *csrVal, const int *csrRowPtr, const int *csrColInd, double *cscVal, int *cscRowInd, int *cscColPtr, cusparseAction_t copyValues, cusparseIndexBase_t idxBase)
 
 
  
--- src/cml/cml_spmat.cuh
+++ src/cml/cml_spmat.cuh
@@ -15,37 +15,36 @@ I ptr_len(const spmat<T, I, O> &mat) {
 
 }
 
-// Maybe store transpose sequentially after (makes factors easier to store).
 template <typename T, typename I, MAT_ORD O>
 struct spmat {
-  T *val_n, *val_t;
-  I *ind_n, *ptr_n, *ind_t, *ptr_t;
+  cusparseMatDescr_t descr;
+  T *val, *val;
+  I *ind, *ptr;
   size_t m, n, nnz;
-  spmat(T *val_n, I *ind_n, I *ptr_n, T *val_t, I *ind_t, I *ptr_t, size_t m,
-        size_t n, size_t nnz) 
-    : val_n(val_n), ind_n(ind_n), ptr_n(ptr_n), val_t(val_t), ind_n(ind_t),
-      ptr_t(ptr_t), m(m), n(n), nnz(nnz) { }
+  spmat(T *val, I *ind, I *ptr, size_t n, size_t nnz) 
+      : val(val), ind(ind), ptr(ptr), m(m), n(n), nnz(nnz) { }
 };
 
-template <typename T, typname I, MAT_ORD O>
+template <typename T, typename I, MAT_ORD O>
 spmat<T, I, O> spmat_alloc(size_t m, size_t n, size_t nnz) {
-  spmat<T, I, O> mat(0, 0, 0, 0, 0, 0, m, n, nnz);
-  cudaMalloc(&mat.val_n, nnz * sizeof(T));
-  cudaMalloc(&mat.ind_n, nnz * sizeof(I));
-  cudaMalloc(&mat.ptr_n, ptr_len(spmat) * sizeof(I));
-  cudaMalloc(&mat.val_t, nnz * sizeof(T));
-  cudaMalloc(&mat.ind_t, nnz * sizeof(I));
-  cudaMalloc(&mat.ptr_t, ptr_len(spmat) * sizeof(I));
+  spmat<T, I, O> mat(0, 0, 0, m, n, nnz);
+  cudaMalloc(&mat.val, 2 * nnz * sizeof(T));
+  cudaMalloc(&mat.ind, 2 * nnz * sizeof(I));
+  cudaMalloc(&mat.ptr, (m + n + 2) * sizeof(I));
   return mat;
 }
 
 
-template <typename T, typname I, MAT_ORD O>
-spmat<T, I, O> spmat_memcpy(spmat<T, I, O> *A, const T *val, const I *ind,
-                            const I *ptr) {
-  cudaMemcpy(A->val, val, A->nnz * sizeof(T));
-  cudaMemcpy(A->ind, ind, A->nnz * sizeof(T));
-  cudaMemcpy(A->ptr, ptr, ptr_len(*A) sizeof(T));
+template <typename T, typname I, typename I, MAT_ORD O>
+spmat<T, I, O> spmat_memcpy(spmat<T, I, O> *A, const T *val, const INT *ind,
+                            const INT *ptr) {
+  cudaMemcpy(A->val, val, A->nnz * sizeof(T), cudaMemcpyHostToDevice);
+  cudaMemcpy(A->ind, ind, A->nnz * sizeof(T), cudaMemcpyHostToDevice);
+  cudaMemcpy(A->ptr, ptr, ptr_len(*A) * sizeof(T), cudaMemcpyHostTodevice);
+  csr2csc(handle_s, m, n, nnz, val_a_d, rptr_a_d, cind_a_d, val_at_d,
+     cind_at_d, rptr_at_d, CUSPARSE_ACTION_NUMERIC,
+     CUSPARSE_INDEX_BASE_ZERO);
+
 }
 
 
