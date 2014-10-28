0da9de343e90ac4a8a8ce5c64cf40cf34460521e
foges <unpluggd@gmail.com>
2014-10-27 22:53:44 -0700

    Sparse matrix generator
    Former-commit-id: d156453c488612d6d4d0bf122aff6b053c18539d

--- examples/cpp_sp/mat_gen.h
+++ examples/cpp_sp/mat_gen.h
@@ -2,7 +2,15 @@
 #define MAT_GEN_H_
 
 #include <stdlib.h>
+
+#include <algorithm>
 #include <cmath>
+#include <vector>
+
+template <typename T>
+inline T rand(T lb, T ub) {
+  return (ub - lb) * (rand() / static_cast<T>(RAND_MAX)) + lb;
+}
 
 template <typename T>
 void MatGen(int m, int n, int nnz, T *val, int *rptr, int *cind, T lb, T ub) {
@@ -15,13 +23,49 @@ void MatGen(int m, int n, int nnz, T *val, int *rptr, int *cind, T lb, T ub) {
     rptr[i] = num;
     for (int j = 0; j < n && num < nnz; ++j) {
       if (rand() / kRandMax * ((kM - i) * kN - j) < (nnz - num)) {
-        val[num] = (ub - lb) * (rand() / kRandMax) + lb;
+        val[num] = rand(lb, ub);
         cind[num] = j;
         num++;
       }
     }
   }
   rptr[m] = nnz;
+}
+
+template <typename T>
+int MatGenApprox(int m, int n, int nnz, T *val, int *rptr, int *cind, T lb,
+                 T ub) {
+  std::vector<std::pair<int, int>> indices;
+  std::vector<T> values;
+
+  indices.reserve(nnz);
+  values.reserve(nnz);
+
+  for (size_t i = 0; i < nnz; ++i) {
+    indices.emplace_back(std::min(m - 1, static_cast<int>(rand(0, m))),
+        std::min(n - 1, static_cast<int>(rand(0, n))));
+    values.push_back(rand(lb, ub));
+  }
+
+  std::sort(indices.begin(), indices.end());
+
+  int row_ind = 1;
+  int col_ind = 1;
+  rptr[0] = 0; 
+  cind[0] = indices[0].second;
+  val[0] = values[0];
+  for (size_t i = 1; i < nnz; ++i) {
+    if (indices[i-1] == indices[i])
+      continue;
+    for (size_t j = indices[i-1].first; j < indices[i].first; ++j)
+      rptr[row_ind++] = col_ind;
+    cind[col_ind] = indices[i].second;
+    val[col_ind++] = values[i];
+  }
+
+  for (size_t j = indices[nnz-1].first; j < m + 1; ++j)
+    rptr[row_ind++] = col_ind;
+  return col_ind;
 }
 
 #endif  // MAT_GEN_H_
