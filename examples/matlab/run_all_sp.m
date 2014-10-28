092133eae91c313aefb6b1956050a4099f317696
foges <chris.fougner@gmail.com>
2014-10-27 21:33:25 -0700

    sparse LP
    Former-commit-id: 570db27abbf3a4dc34bb43f951682ce843d62763

---
+++ examples/matlab/run_all_sp.m
@@ -0,0 +1,17 @@
+%RUN Runs all examples.
+
+% Build interface
+cd([pwd '/../../src/interface_matlab'])
+pogs_setup -gpu % Alternatively `pogs_setup gpu`
+cd([pwd '/../../examples/matlab/'])
+addpath([pwd '/../../src/interface_matlab'])
+
+%% Run examples
+
+% fprintf('\nLasso\n')
+% t = lasso_sp(100, 1000, 1e4, struct('abs_tol', 1e-5, 'rel_tol', 1e-4, 'max_iter', 1001))
+% fprintf('Solver Time: %e sec\n', t)
+%%
+fprintf('\nLP-Equality\n')
+t = lp_eq_sp(200, 1000, 1e4, struct('abs_tol', 1e-4, 'rel_tol', 1e-3, 'max_iter', 2001))
+fprintf('Solver Time: %e sec\n', t)
