f39f37452f9eb044a591a1bb04b2f21b8b49ec64
foges <unpluggd@gmail.com>
2015-02-10 23:56:44 -0800

    projector helper added
    Former-commit-id: f4a7c1e65604bbf20d3952913696e64fdaad06e7

---
+++ src/gpu/include/projector_helper.cuh
@@ -0,0 +1,44 @@
+#ifndef PROJECTOR_HELPER_CUH_
+#define PROJECTOR_HELPER_CUH_
+
+#include <limits>
+
+#include "cml/cml_blas.cuh" 
+#include "cml/cml_vector.cuh" 
+#include "matrix/matrix.h" 
+#include "util.cuh" 
+
+// Check that (x, y) satisfies
+//   1. y = Ax
+//   2. x = min. ||Ax - y0||_2^2 + s||x - x0||_2^2
+template <typename T>
+void CheckProjection(const Matrix<T> *A, const T *x, const T *y, T s) {
+  T tol = 1e2 * std::numeric_limits<T>::epsilon();
+  cml::vector<T> x_ = cml::vector_calloc<T>(A->Cols());
+  cml::vector<T> y_ = cml::vector_calloc<T>(A->Rows());
+  
+  // Check residual
+  cml::vector_memcpy(&x_, x);
+  cml::vector_memcpy(&y_, y);
+  A->Mul('n', static_cast<T>(1.), x_.data, static_cast<T>(-1.), y_.data);
+  cudaDeviceSynchronize();
+  T nrm_r = cml::blas_nrm2(hdl, &y_)  / std::sqrt(A->Rows());
+  DEBUG_EXPECT_EQ_EPS(nrm_r, static_cast<T>(0.), tol);
+
+  // Check KKT
+  cml::vector_memcpy(&x_, x);
+  cml::vector_memcpy(&y_, y0);
+  A->Mul('n', static_cast<T>(1.), x_.data, static_cast<T>(-1.), y_.data);
+  cudaDeviceSynchronize();
+  A->Mul('t', static_cast<T>(1.), y_.data, s, x_.data);
+  cudaDeviceSynchronize();
+  cml::blas_axpy(hdl, -s, &x0_vec, &x_);
+  T nrm_kkt = cml::blas_nrm2(hdl, &x_) / std::sqrt(A->Cols());
+  DEBUG_EXPECT_EQ_EPS(nrm_kkt, static_cast<T>(0.), tol);
+
+  cml::vector_free(&x_);
+  cml::vector_free(&y_);
+}
+
+#endif  // PROJECTOR_HELPER_CUH_
+
