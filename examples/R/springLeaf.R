29ba97f0254c21ddc1cc67cb0289e3fafba10598
Arno Candel <arno.candel@gmail.com>
2017-03-17 18:51:33 -0700

    Minor cosmetics - add regression.
    Former-commit-id: 1a425310a5ddb1d06592fdb682490653d1535fc3

--- examples/R/springLeaf.R
+++ examples/R/springLeaf.R
@@ -3,11 +3,15 @@ library(pogs)
 library(glmnet)
 library(data.table)
 
-# kaggle springleaf
-N<-10000
+N<-100000
 H <- 0.8*N
+f <- "/tmp/train.csv" # kaggle springleaf
 response <- 'target'
-f <- "/tmp/train.csv"
+family <- "gaussian"
+#family <- "binomial"
+pogs<-TRUE
+glmnet<-FALSE
+h2o<-TRUE
 
 
 ## DATA PREP
@@ -23,52 +27,75 @@ h2o.init(nthreads=-1)
 df.hex <- h2o.importFile(file)
 
 train.hex <- df.hex[1:H,]
-train.hex[[response]] <- as.factor(train.hex[[response]])
 valid.hex <- df.hex[(H+1):N,]
-valid.hex[[response]] <- as.factor(valid.hex[[response]])
+if (family=="binomial") {
+  train.hex[[response]] <- as.factor(train.hex[[response]])
+  valid.hex[[response]] <- as.factor(valid.hex[[response]])
+}
 
 train <- df[1:H,]
 valid <- df[(H+1):N,]
 cols <- setdiff(names(df), c(response))
 
 train_x  <- as.matrix(as.data.frame(train[,cols,with=FALSE]))
-train_y  <- as.vector(train[[response]])
+train_y  <- as.numeric(as.vector(train[[response]]))
 valid_x  <- as.matrix(as.data.frame(valid[,cols,with=FALSE]))
-valid_y  <- as.vector(valid[[response]])
+valid_y  <- as.numeric(as.vector(valid[[response]]))
+
+
+
 
 ## POGS GPU
-s1 <- proc.time()
-pogs = pogsnet(x = train_x, y = train_y, family = "binomial", alpha = 0.5, nlambda=2)
-e1 <- proc.time()
-pogs_pred_y = predict(pogs, valid_x, type="response")
+if (pogs) {
+  s1 <- proc.time()
+  pogs = pogsnet(x = train_x, y = train_y, family = family, alpha = 0.5) #TODO: Disable lambda search (no longer use nlambda=100)
+  e1 <- proc.time()
+  pogs_pred_y = predict(pogs, valid_x, type="response")
+  pogs_pred_y = pogs_pred_y / norm(pogs_pred_y)
 
-print("POGS GPU: ")
-print(e1-s1)
-pogspreds <- as.h2o(pogs_pred_y)
-h2o.auc(h2o.make_metrics(pogspreds[,1],   valid.hex[[response]]))
+  print("POGS GPU: ")
+  print(e1-s1)
+  pogspreds <- as.h2o(pogs_pred_y)
+  if (family == "gaussian") {
+    h2o.rmse(h2o.make_metrics(pogspreds[,1], valid.hex[[response]]))
+  } else {
+    h2o.auc(h2o.make_metrics(pogspreds[,1], valid.hex[[response]]))
+  }
+}
 
 
 
 ## GLMNET
-s2 <- proc.time()
-glmnet = glmnet(x = train_x, y = train_y, family = "binomial", alpha = 0.5, lambda=c(1e-5))
-e2 <- proc.time()
-glmnet_pred_y = predict(glmnet, valid_x, type="response")
+if (glmnet) {
+  s2 <- proc.time()
+  glmnet = glmnet(x = train_x, y = train_y, family = family, alpha = 0.5, lambda=c(1e-5))
+  e2 <- proc.time()
+  glmnet_pred_y = predict(glmnet, valid_x, type="response")
 
-print("GLMNET CPU")
-print(e2-s2)
-glmnetpreds <- as.h2o(glmnet_pred_y)
-h2o.auc(h2o.make_metrics(glmnetpreds[,1], valid.hex[[response]]))
+  print("GLMNET CPU")
+  print(e2-s2)
+  glmnetpreds <- as.h2o(glmnet_pred_y)
+  if (family == "gaussian") {
+    h2o.rmse(h2o.make_metrics(glmnetpreds[,1], valid.hex[[response]]))
+  } else {
+    h2o.auc(h2o.make_metrics(glmnetpreds[,1], valid.hex[[response]]))
+  }
+}
 
 
 
 ## H2O
+if (h2o) {
+  s3 <- proc.time()
+  h2omodel <- h2o.glm(x=cols, y=response, training_frame=train.hex, family=family, alpha = 0.5)
+  e3 <- proc.time()
+  h2opreds <- h2o.predict(h2omodel, valid.hex)
 
-s3 <- proc.time()
-h2omodel <- h2o.glm(x=cols, y=response, training_frame=train.hex, family="binomial", lambda=c(1e-5))
-e3 <- proc.time()
-h2opreds <- h2o.predict(h2omodel, valid.hex)
-
-print("H2O CPU ")
-print(e3-s3)
-h2o.auc(h2o.make_metrics(h2opreds[,3],    valid.hex[[response]]))
+  print("H2O CPU ")
+  print(e3-s3)
+  if (family == "gaussian") {
+    h2o.rmse(h2o.make_metrics(h2opreds[,1], valid.hex[[response]]))
+  } else {
+    h2o.auc(h2o.make_metrics(h2opreds[,3], valid.hex[[response]]))
+  }
+}
