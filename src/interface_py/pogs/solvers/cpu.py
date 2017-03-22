fb602db0d30c6794334025221c0a8becf20b5299
Arno Candel <arno.candel@gmail.com>
2017-03-21 17:59:04 -0700

    Revert "removed python interface (migrating to pogs-python repo)"
    This reverts commit 1d93067be40a8acaf2b20198e0a43ecd8ffc1da3 [formerly 2958fecf33b0e38437d47cb772bf0495204e64ea].
    
    
    Former-commit-id: 70f4ac39eeb8a5f35ec673c7d50fa3f7eca6605c

---
+++ src/interface_py/pogs/solvers/cpu.py
@@ -0,0 +1,26 @@
+from pogs.libs.cpu import pogsCPU
+from pogs.solvers.base import BaseSolver
+
+
+if not pogsCPU:
+	print '\nWarning: Cannot create a POGS CPU Solver instance without linking Python module to a compiled POGS CPU libirary'
+	print '> Setting pogs.SolverCPU=None'
+	SolverCPU=None
+else:
+	class SolverCPU(object):
+		def __init__(self, A, **kwargs):
+			self.solver = BaseSolver(A,pogsCPU)
+			self.info=self.solver.info
+			self.solution=self.solver.pysolution
+
+		def init(self, A, **kwargs):
+			self.solver.init(A,**kwargs)
+		 
+		def solve(self, f, g, **kwargs):
+			self.solver.solve(f,g,**kwargs)
+		 
+		def finish(self):
+			self.solver.finish()
+
+		def __delete__(self):
+			self.solver.finish()
