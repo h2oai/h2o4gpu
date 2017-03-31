766f52b976b5de130d0100a75a29a4eff5cf1687
Arno Candel <arno.candel@gmail.com>
2017-03-31 13:59:24 -0700

    Disable exact_stop
    Former-commit-id: c3e15b1a48f4a204d2e139b6312417973657e9ff

--- src/gpu/pogs.cu
+++ src/gpu/pogs.cu
@@ -123,7 +123,7 @@ PogsStatus Pogs<T, M, P>::Solve(const std::vector<FunctionObj<T> > &f,
   const T kProjTolMin = static_cast<T>(1e-2); // Projection tolerance
   const T kProjTolPow = static_cast<T>(1.3); // Projection tolerance
   const T kProjTolIni = static_cast<T>(1e-5); // Projection tolerance
-  bool use_exact_stop = true;
+  bool use_exact_stop = false;
 
   //  PUSH_RANGE("PogsSolve",PogsSolve,1);
 
