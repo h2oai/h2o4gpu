1985857d277bbca4bbd7738db13276587c430cd0
pseudotensor <pseudotensor@gmail.com>
2017-04-05 02:45:17 -0700

    merged Arno's alpha parameter and f,g setups into the new multi-gpu code.
    Former-commit-id: 86680d4aeaa4ef2fdb64aebf15e88593459db270

--- examples/cpp/lasso_path.openmp.cpp
+++ examples/cpp/lasso_path.openmp.cpp
@@ -42,6 +42,23 @@ T Asum(std::vector<T> *v) {
 template <typename T>
 double LassoPath(size_t m, size_t n) {
   int nlambda = 100;
+  // number of openmp threads = number of cuda devices to use
+  int nGPUs=2;
+
+#ifdef _OPENMP
+  int nopenmpthreads0=omp_get_max_threads();
+#define MIN(a,b) ((a)<(b)?(a):(b))
+  omp_set_num_threads(MIN(nopenmpthreads0,nGPUs));
+  int nopenmpthreads=omp_get_max_threads();
+  nGPUs = nopenmpthreads; // openmp threads = cuda devices used
+  fprintf(stdout,"Number of original threads=%d.  Number of threads for cuda=%d\n",nopenmpthreads0,nopenmpthreads);
+
+  
+#else
+#error Need OpenMP
+#endif
+
+  // allocate matrix problem to solve
   std::vector<T> A(m * n);
   std::vector<T> b(m);
   std::vector<T> x_last(n, std::numeric_limits<T>::max());
@@ -53,7 +70,7 @@ double LassoPath(size_t m, size_t n) {
   fprintf(stdout,"END FILL DATA\n");
 
 
-  // constant across openmp threads  
+  // set lambda max
   T lambda_max = static_cast<T>(0);
   for (unsigned int j = 0; j < n; ++j) {
     T u = 0;
@@ -63,50 +80,23 @@ double LassoPath(size_t m, size_t n) {
     lambda_max = std::max(lambda_max, std::abs(u));
   }
 
-  // number of openmp threads = number of cuda devices to use
-  int nDevall=2;
 
-#ifdef _OPENMP
-  int nopenmpthreads0=omp_get_max_threads();
-#define MIN(a,b) ((a)<(b)?(a):(b))
-  omp_set_num_threads(MIN(nopenmpthreads0,nDevall));
-  int nopenmpthreads=omp_get_max_threads();
-  nDevall = nopenmpthreads; // openmp threads = cuda devices used
-  fprintf(stdout,"Number of original threads=%d.  Number of threads for cuda=%d\n",nopenmpthreads0,nopenmpthreads);
 
-  
-#else
-#error Need OpenMP
-#endif
 
   
   // Set up pogs datastructures: A_, pogs_data, f, g as pointers
-  // Require pointers, because don't want to call consructor yet as that allocates cuda memory, etc.
-  fprintf(stderr,"MatrixDense\n"); fflush(stderr);
-  std::vector<std::unique_ptr<pogs::MatrixDense<T> > > A_(nDevall);
-  fprintf(stderr,"PogsDirect\n"); fflush(stderr);
-  std::vector<std::unique_ptr<pogs::PogsDirect<T, pogs::MatrixDense<T> > > > pogs_data(nDevall);
-  fprintf(stderr,"f\n"); fflush(stderr);
-  std::vector<std::vector<FunctionObj<T> > > f(nDevall);
-  fprintf(stderr,"g\n"); fflush(stderr);
-  std::vector<std::vector<FunctionObj<T> > > g(nDevall);
+  // Require pointers, because don't want to call constructor yet as that allocates cuda memory, etc.
+  std::vector<std::unique_ptr<pogs::MatrixDense<T> > > A_(nGPUs);
+  std::vector<std::unique_ptr<pogs::PogsDirect<T, pogs::MatrixDense<T> > > > pogs_data(nGPUs);
 
-  
-  
-  //#pragma omp parallel for
-  for(int i=0;i<nDevall;i++){
 
-    fprintf(stderr,"MatrixDense Assign: %d\n",i); fflush(stderr);
+  // create class objects that creates cuda memory, cpu memory, etc.
+  // Avoid doing for each alpha or lambda
+  for(int i=0;i<nGPUs;i++){
+
     A_[i] = std::unique_ptr<pogs::MatrixDense<T> >(new pogs::MatrixDense<T>(i, 'r', m, n, A.data() ));
-    fprintf(stderr,"PogsDirect Assign: %d\n",i); fflush(stderr);
     pogs_data[i] = std::unique_ptr<pogs::PogsDirect<T, pogs::MatrixDense<T> > >(new pogs::PogsDirect<T, pogs::MatrixDense<T> >(i, *(A_[i]) ));
-    
-    fprintf(stderr,"f assign: %d\n",i); fflush(stderr);
-    for (unsigned int j = 0; j < m; ++j) f[i].emplace_back(kSquare, static_cast<T>(1), b[j]);
-    fprintf(stderr,"g assign: %d\n",i); fflush(stderr);
-    for (unsigned int j = 0; j < n; ++j) g[i].emplace_back(kAbs);
 
-    
     pogs_data[i]->SetnDev(1); // set how many cuda devices to use internally in pogs
     pogs_data[i]->SetwDev(i); // set which cuda device to use
 
@@ -126,30 +116,42 @@ double LassoPath(size_t m, size_t n) {
     }
   }
 
+  
   fprintf(stdout,"BEGIN SOLVE\n"); double t = timer<double>();
 #pragma omp parallel for
-  for (int i = 0; i < nlambda; ++i){
-
-    // starts at lambda_max and goes down to 1E-2 lambda_max in exponential spacing
-    T lambda = std::exp((std::log(lambda_max) * ((float)nlambda - 1.0f - (float)i) +
-                         static_cast<T>(1e-2) * std::log(lambda_max) * (float)i) / ((float)nlambda - 1.0f));
-
-    // choose cuda device
-    int wDev=i%nDevall;
-    //int wDev=0;
-
-    fprintf(stderr,"i=%d lambda=%g wDev=%d\n",i,lambda,wDev);
-    // assign lambda
-    for (unsigned int j = 0; j < n; ++j) g[wDev][j].c = lambda;
+  for (int a = 0; a < N; ++a) { //alpha search FIXME: enable alpha=1 (a==N) once we have L1
+    int me = omp_get_thread_num();
+    double alpha = (double)a/N;
     
-    pogs_data[wDev]->Solve(f[wDev], g[wDev]);
+    // setup f,g as functions of alpha
+    std::vector<FunctionObj<T> > f;
+    std::vector<FunctionObj<T> > g;
+    for (unsigned int j = 0; j < m; ++j) f.emplace_back(kSquare, static_cast<T>(1-alpha), b[j]);
+    for (unsigned int j = 0; j < n; ++j) g.emplace_back(kAbs);
     
-    //      std::vector<T> x(n);
-    //      for (unsigned int j = 0; j < n; ++j) x[j] = pogs_data[wDev]->GetX()[j];
-    ///    if (MaxDiff(&x, &x_last) < 1e-3 * Asum(&x))
-    //      break;
-    //x_last = x;
-  }
+    
+    for (int i = 0; i < nlambda; ++i){
+      
+      // starts at lambda_max and goes down to 1E-2 lambda_max in exponential spacing
+      T lambda = std::exp((std::log(lambda_max) * ((float)nlambda - 1.0f - (float)i) +
+                           static_cast<T>(1e-2) * std::log(lambda_max) * (float)i) / ((float)nlambda - 1.0f));
+      
+      fprintf(stderr,"me=%d a=%d alpha=%g i=%d lambda=%g me=%d\n",me,a,alpha,i,lambda);
+
+      // assign lambda
+      for (unsigned int j = 0; j < n; ++j) g[j].c = lambda;
+
+      // Solve
+      pogs_data[me]->Solve(f, g);
+      
+      //      std::vector<T> x(n);
+      //      for (unsigned int j = 0; j < n; ++j) x[j] = pogs_data[me]->GetX()[j];
+      ///    if (MaxDiff(&x, &x_last) < 1e-3 * Asum(&x))
+      //      break;
+      //x_last = x;
+    }// over lambda
+  }// over alpha
+
   double tf = timer<double>();
   fprintf(stdout,"END SOLVE: type 1 m %d n %d tfd %g ts %g\n",(int)m,(int)n,t1-t0,tf-t);
 
