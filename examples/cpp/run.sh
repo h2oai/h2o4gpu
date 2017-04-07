a66d77cd941744b137b6a816fc2c55dd8f31e171
Arno Candel <arno.candel@gmail.com>
2017-04-07 03:01:57 -0700

    Add results for 40-core CPU.
    Former-commit-id: ad56b92c6fb39494ea4682caad938956c58d2452

--- examples/cpp/run.sh
+++ examples/cpp/run.sh
@@ -6,13 +6,13 @@ DIR=$HOSTNAME.$L.$A.$V.`git reflog | head -n1 | awk '{print $1}'`
 
 rm -f me*.txt
 mkdir -p $DIR
-make gpualt
+#make gpualt
 make cpualt
-for i in 16 8 4 2 1; do
-   mkdir -p $DIR/gpu.$i
-   ./h2oai-glm-gpu $i $L $A $V 2>&1 | tee $DIR/gpu.$i/pogs.$DIR.gpu.$i.log
-   mv me*txt $DIR/gpu.$i/
-done
+#for i in 16 8 4 2 1; do
+#   mkdir -p $DIR/gpu.$i
+#   ./h2oai-glm-gpu $i $L $A $V 2>&1 | tee $DIR/gpu.$i/pogs.$DIR.gpu.$i.log
+#   mv me*txt $DIR/gpu.$i/
+#done
 mkdir -p $DIR/cpu
 ./h2oai-glm-cpu 1 $L $A $V 2>&1 | tee $DIR/cpu/pogs.$DIR.cpu.log
 mv me*txt $DIR/cpu/
