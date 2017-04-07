094f116a86d97ba3c58e8492dc68dc802639b023
Arno Candel <arno.candel@gmail.com>
2017-04-06 20:32:31 -0700

    Add run script for EC2 16-node.
    Former-commit-id: 7c41d264d776a6d4275accb9c2cd24816f8eeb0d

---
+++ examples/cpp/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ec2.100.16.0.2
+git reflog | head -n1 | awk '{print $1}' > ec2.100.16.0.2/sha
+for i in 16 8 4 2 1; do
+   mkdir -p ec2.100.16.0.2/gpu.$i
+   ./h2oai-glm-gpu $i 100 16 0.2 2>&1 | tee ec2.100.16.0.2/gpu.$i/pogs.ec2.100.16.0.2.gpu.$i.log
+   mv me*txt ec2.100.16.0.2/gpu.$i/
+done
+mkdir -p ec2.100.16.0.2/cpu
+./h2oai-glm-cpu 1 100 16 0.2 2>&1 | tee ec2.100.16.0.2/cpu/pogs.ec2.100.16.0.2.cpu.log
+mv me*txt ec2.100.16.0.2/cpu/
+
