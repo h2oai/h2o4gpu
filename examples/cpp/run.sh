81a3b360bb651c542418c5d4c9577a4d045575cc
Arno Candel <arno.candel@gmail.com>
2017-04-06 20:33:24 -0700

    Add make
    Former-commit-id: 63ca2cdbd28a0c954834478fa718fd77a53a638c

--- examples/cpp/run.sh
+++ examples/cpp/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p ec2.100.16.0.2
 git reflog | head -n1 | awk '{print $1}' > ec2.100.16.0.2/sha
+make gpualt
+make cpualt
 for i in 16 8 4 2 1; do
    mkdir -p ec2.100.16.0.2/gpu.$i
    ./h2oai-glm-gpu $i 100 16 0.2 2>&1 | tee ec2.100.16.0.2/gpu.$i/pogs.ec2.100.16.0.2.gpu.$i.log
