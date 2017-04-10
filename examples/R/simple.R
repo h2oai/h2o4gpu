88deb4204555cc061c53d7926527898329ab5e4f
Arno Candel <arno.candel@gmail.com>
2017-04-10 13:55:53 -0700

    Update R "unit test"
    Former-commit-id: 264776380d1e7d77156a7114326e1fd8ffc1b882

--- examples/R/simple.R
+++ examples/R/simple.R
@@ -1,17 +1,19 @@
 library(pogs)
-#library(glmnet)
+library(glmnet)
 
 age     <- c(4, 8, 7, 12, 6, 9, 10, 14, 7)
 gender  <- as.factor(c(1, 0, 1, 1, 1, 0, 1, 0, 0))
 bmi_p   <- c(0.86, 0.45, 0.99, 0.84, 0.85, 0.67, 0.91, 0.29, 0.88)
 m_edu   <- as.factor(c(0, 1, 1, 2, 2, 3, 2, 0, 1))
 p_edu   <- as.factor(c(0, 2, 2, 2, 2, 3, 2, 0, 0))
-f_color <- as.factor(c("blue", "blue", "yellow", "red", "red", "yellow",
-                       "yellow", "red", "yellow"))
-                       asthma <- c(1, 1, 0, 1, 0, 0, 0, 1, 1)
+f_color <- as.factor(c("blue", "blue", "yellow", "red", "red", "yellow", "yellow", "red", "yellow"))
+asthma <- c(1, 1, 0, 1, 0, 0, 0, 1, 1)
 
 xfactors <- model.matrix(asthma ~ gender + m_edu + p_edu + f_color)[, -1]
 x        <- as.matrix(data.frame(age, bmi_p, xfactors))
 
-model = pogsnet(x = x, y = asthma, family = "binomial", alpha = 1)
-#model = glmnet(x = x, y = asthma, family = "binomial", alpha = 1)
+model = pogsnet(x = x, y = asthma, family = "gaussian", alpha = 1)
+print(paste0("POGS:",mean((predict(model, x)-asthma)^2)))
+
+model = glmnet(x = x, y = asthma, family = "gaussian", alpha = 1)
+print(paste0("GLMNET:",mean((predict(model, x)-asthma)^2)))
