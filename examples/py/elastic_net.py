eb8c69e2fd60055067b0de8ce9f113aaa02a5c60
Arno Candel <arno.candel@gmail.com>
2017-04-12 16:28:52 -0700

    Add Jupyter notebook for Py GLM (H2OAI/glmnet). Add sym link to feather file created by R.
    Former-commit-id: 7b6da5aa93aa8833eaaff8934ac4d5c12795a5c3
    Former-commit-id: 7a83710c1b38e69cc95a50f7fe6691216add7cf4

---
+++ examples/py/elastic_net.py
@@ -0,0 +1,85 @@
+import pogs as pogs
+import scipy as sp
+import numpy as np
+from numpy import abs, exp, float32, float64, log, max, sum, zeros
+from numpy.random import rand, randn
+
+
+'''
+Elastic Net
+
+   minimize    (1/2) ||Ax - b||_2^2 + \alpha * \lambda ||x||_1 + 0.5 * (1-\alpha) * \lambda ||x||_2
+
+   for 100 values of \lambda, and alpha in [0,1]
+   See <pogs>/matlab/examples/lasso_path.m for detailed description.
+'''
+
+def ElasticNet(X, y, gpu=True, double_precision=False, nlambda=100, alpha=0.5):
+  # set solver cpu/gpu according to input args
+  if gpu and pogs.SolverGPU is None:
+    print("\nGPU solver unavailable, using CPU solver\n")
+    gpu=False
+
+  Solver = pogs.SolverGPU if gpu else pogs.SolverCPU
+
+  # cast A as float/double according to input args
+  A=np.array(X, dtype='float64') if double_precision else np.array(X, dtype='float32')
+  m = A.shape[0]
+  n = A.shape[1]
+
+  b=y
+
+  # lambda_max
+  lambda_max = max(abs(A.T.dot(b)))
+
+
+  # f(Ax) = ||Ax - b||_2^2
+  f = pogs.FunctionVector(m,double_precision=double_precision)
+  f.b[:]=b[:]
+  f.h[:]=pogs.FUNCTION["SQUARE"]
+
+  # g(x) = 0.2*lambda_max*||x||_1
+  g = pogs.FunctionVector(n,double_precision=double_precision)
+  g.a[:] = 1
+  g.h[:] = pogs.FUNCTION["ABS"]
+
+
+  # store results for comparison 
+  x_prev = zeros(n)
+
+  # timer
+  runtime = 0.
+
+  #s = Solver(sp.sparse.csr_matrix(A)) ##TODO: compare to sparse
+  #s = Solver(sp.sparse.csc_matrix(A))
+  s = Solver(A)
+
+  for i in range(nlambda):
+    _lambda= exp( (log(lambda_max)*(nlambda-1-i)+1e-2*log(lambda_max)*i )/ (nlambda-1))
+
+    g.c[:]=    alpha*_lambda
+    g.e[:]=(1-alpha)*_lambda
+
+    # solve ##TODO: let C++ do the lambda-path
+    s.solve(f, g)
+
+    # add run time
+    runtime += s.info.solvetime
+
+    # copy
+    x_curr=s.solution.x
+
+    # check stopping condition
+#    if max(abs(x_prev-x_curr)) < 1e-3* sum(abs(x_curr)):
+#      break
+    
+    x_prev[:]=x_curr[:]
+
+
+  # tear down solver in C++/CUDA
+  s.finish()
+
+  return runtime
+
+if __name__ == "__main__":
+   print("Cannot run in standalone mode.")
