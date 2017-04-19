0b8bb58143ed6132ea3fa6fef7d2a678fc18f22c
Arno Candel <arno.candel@gmail.com>
2017-04-18 17:10:02 -0700

    Go back to simple csv
    Former-commit-id: 5420a4a58fb04ad89e3436f72ae5caa4d8f5bbd9
    Former-commit-id: 73cb778d6176603ac9564e9ecc9304ad47eb9ac7

--- examples/R/simple2.R
+++ examples/R/simple2.R
@@ -1,25 +1,26 @@
 library(pogs)
-alpha <- 0
+alpha <- 0.5
 
-if (FALSE) {
-age     <- c(4, 8, 7, 12, 6, 9, 10, 14, 7)
-gender  <- as.factor(c(1, 0, 1, 1, 1, 0, 1, 0, 0))
-bmi_p   <- c(0.86, 0.45, 0.99, 0.84, 0.85, 0.67, 0.91, 0.29, 0.88)
-m_edu   <- as.factor(c(0, 1, 1, 2, 2, 3, 2, 0, 1))
-p_edu   <- as.factor(c(0, 2, 2, 2, 2, 3, 2, 0, 0))
-f_color <- as.factor(c("blue", "blue", "yellow", "red", "red", "yellow", "yellow", "red", "yellow"))
-asthma <- c(1, 1, 0, 1, 0, 0, 0, 1, 1)
-y <- asthma
+if (TRUE) {
+  age     <- c(4, 8, 7, 12, 6, 9, 10, 14, 7)
+  gender  <- as.factor(c(1, 0, 1, 1, 1, 0, 1, 0, 0))
+  bmi_p   <- c(0.86, 0.45, 0.99, 0.84, 0.85, 0.67, 0.91, 0.29, 0.88)
+  m_edu   <- as.factor(c(0, 1, 1, 2, 2, 3, 2, 0, 1))
+  p_edu   <- as.factor(c(0, 2, 2, 2, 2, 3, 2, 0, 0))
+  f_color <- as.factor(c("blue", "blue", "yellow", "red", "red", "yellow", "yellow", "red", "yellow"))
+  asthma <- c(1, 1, 0, 1, 0, 0, 0, 1, 1)
+  y <- asthma
 
 
-xfactors <- model.matrix(asthma ~ gender + m_edu + p_edu + f_color)[, -1]
-x        <- as.matrix(data.frame(age, bmi_p, xfactors))
+  xfactors <- model.matrix(asthma ~ gender + m_edu + p_edu + f_color)[, -1]
+  x        <- as.matrix(data.frame(age, bmi_p, xfactors))
+} else {
+  df = read.csv("iris.csv")
+  x = as.matrix(df[,1:3])
+  y = df[,4]
 }
 
 
-df = read.csv("iris.csv")
-x = as.matrix(df[,1:3])
-y = df[,4]
 print(x)
 print(y)
 
