4cc1c8770b30a2825a0a786c1ad3feeae305631d
pseudotensor <pseudotensor@gmail.com>
2017-04-26 23:06:34 -0400

    tab vs. space issue
    Former-commit-id: 1cd30ea2e3df8f67f196450a33744beea47c0ac8
    Former-commit-id: eb9e5dac007149767c54f34f61a387f1b2237357

--- src/interface_py/pogs/libs/gpu.py
+++ src/interface_py/pogs/libs/gpu.py
@@ -8,38 +8,38 @@ lib_path = os.path.join(os.path.dirname(__file__),"../../cpogs_gpu" + ext)
 
 
 try:
-	pogsGPU = CDLL(lib_path)
+      pogsGPU = CDLL(lib_path)
 
-	#argument types
-	pogsGPU.pogs_init_dense_single.argtypes = [c_int, c_int, c_size_t, c_size_t, c_float_p]
-	pogsGPU.pogs_init_dense_double.argtypes = [c_int, c_int, c_size_t, c_size_t, c_double_p]
-	pogsGPU.pogs_init_sparse_single.argtypes = [c_int, c_int, c_size_t, c_size_t, c_size_t, c_float_p, c_int_p, c_int_p]
-	pogsGPU.pogs_init_sparse_double.argtypes = [c_int, c_int, c_size_t, c_size_t, c_size_t, c_double_p, c_int_p, c_int_p]
-	pogsGPU.pogs_solve_single.argtypes = [c_void_p, settings_s_p, solution_s_p, info_s_p,
-										c_float_p, c_float_p, c_float_p, c_float_p, c_float_p, c_int_p,
-										c_float_p, c_float_p, c_float_p, c_float_p, c_float_p, c_int_p]
-	pogsGPU.pogs_solve_double.argtypes = [c_void_p, settings_d_p, solution_d_p, info_d_p,
-										c_double_p, c_double_p, c_double_p, c_double_p, c_double_p, c_int_p,
-										c_double_p, c_double_p, c_double_p, c_double_p, c_double_p, c_int_p]
-	pogsGPU.pogs_finish_single.argtypes = [c_void_p]
-	pogsGPU.pogs_finish_double.argtypes = [c_void_p]
+      #argument types
+      pogsGPU.pogs_init_dense_single.argtypes = [c_int, c_int, c_size_t, c_size_t, c_float_p]
+      pogsGPU.pogs_init_dense_double.argtypes = [c_int, c_int, c_size_t, c_size_t, c_double_p]
+      pogsGPU.pogs_init_sparse_single.argtypes = [c_int, c_int, c_size_t, c_size_t, c_size_t, c_float_p, c_int_p, c_int_p]
+      pogsGPU.pogs_init_sparse_double.argtypes = [c_int, c_int, c_size_t, c_size_t, c_size_t, c_double_p, c_int_p, c_int_p]
+      pogsGPU.pogs_solve_single.argtypes = [c_void_p, settings_s_p, solution_s_p, info_s_p,
+                                                            c_float_p, c_float_p, c_float_p, c_float_p, c_float_p, c_int_p,
+                                                            c_float_p, c_float_p, c_float_p, c_float_p, c_float_p, c_int_p]
+      pogsGPU.pogs_solve_double.argtypes = [c_void_p, settings_d_p, solution_d_p, info_d_p,
+                                                            c_double_p, c_double_p, c_double_p, c_double_p, c_double_p, c_int_p,
+                                                            c_double_p, c_double_p, c_double_p, c_double_p, c_double_p, c_int_p]
+      pogsGPU.pogs_finish_single.argtypes = [c_void_p]
+      pogsGPU.pogs_finish_double.argtypes = [c_void_p]
 
 
 
-	#return types
-	pogsGPU.pogs_init_dense_single.restype = c_void_p
-	pogsGPU.pogs_init_dense_double.restype = c_void_p
-	pogsGPU.pogs_init_sparse_single.restype = c_void_p
-	pogsGPU.pogs_init_sparse_double.restype = c_void_p
-	pogsGPU.pogs_solve_single.restype = c_int
-	pogsGPU.pogs_solve_double.restype = c_int
-	pogsGPU.pogs_finish_single.restype = None
-	pogsGPU.pogs_finish_double.restype = None
+      #return types
+      pogsGPU.pogs_init_dense_single.restype = c_void_p
+      pogsGPU.pogs_init_dense_double.restype = c_void_p
+      pogsGPU.pogs_init_sparse_single.restype = c_void_p
+      pogsGPU.pogs_init_sparse_double.restype = c_void_p
+      pogsGPU.pogs_solve_single.restype = c_int
+      pogsGPU.pogs_solve_double.restype = c_int
+      pogsGPU.pogs_finish_single.restype = None
+      pogsGPU.pogs_finish_double.restype = None
 
-	print('\nLoaded POGS GPU library.')
+      print('\nLoaded POGS GPU library.')
 except:
-        logging.exception("in gpu.py")
-	print('\nWarning: POGS GPU shared object (dynamic library) not found at ' + lib_path)
-	pogsGPU=None
+      logging.exception("in gpu.py")
+      print('\nWarning: POGS GPU shared object (dynamic library) not found at ' + lib_path)
+      pogsGPU=None
 
 
