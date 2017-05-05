235dfa22d534ac080c24d3adbb9e148dde46f241
pseudotensor <pseudotensor@gmail.com>
2017-05-05 18:41:43 -0400

    timer
    Former-commit-id: cbb62fde746346ac9a4cba3c06166fd13ab0237d
    Former-commit-id: 4c02294b8964f9e95f0c47a70d5b13c6fafe11c8

--- src/gpu/matrix/matrix_dense.cu
+++ src/gpu/matrix/matrix_dense.cu
@@ -381,7 +381,7 @@ MatrixDense<T>::MatrixDense(int sharedA, int me, int wDev, int datatype, char or
     if(_weight==NULL){
       fprintf(stderr,"datatype=1: making up unity weights: %d %p\n",m,&_weight); fflush(stderr);
       CUDACHECK(cudaMalloc(&_weight, m * sizeof(T))); // allocate on GPU
-      thrust::device_ptr<T> dev_ptr(thrust::device_pointer_cast(_weight));
+      thrust::device_ptr<T> dev_ptr = thrust::device_pointer_cast(&_weight[0]);
       T fill_value=1.0;
       thrust::fill(dev_ptr, dev_ptr + m, fill_value);
     }
