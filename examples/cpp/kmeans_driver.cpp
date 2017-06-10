e02063ca54efe86cd483d779ca22e2637c87e02e
Arno Candel <arno.candel@gmail.com>
2017-06-09 18:37:15 -0700

    WIP towards getting C++ unit test to run with given data.
    Former-commit-id: 74119fafa4eae239b79ec5569b38bb6788699727
    Former-commit-id: 6fd92a2f441038b2ae55c7b230a2a0dfdcbcef0a

--- examples/cpp/kmeans_driver.cpp
+++ examples/cpp/kmeans_driver.cpp
@@ -30,7 +30,16 @@ int main(int argc, char **argv) {
   size_t rows = 260753;  // rows
   size_t cols = 298;  // cols
 
-  h2oaikmeans::H2OAIKMeans<real_t>(&a, k, rows, cols).Solve();
+  void* res = 0;
+  int n_gpu=2;
+  std::vector<real_t> data(rows*cols);
+
+  // works - makes random data
+  //h2oaikmeans::H2OAIKMeans<real_t>(&a, k, rows, cols).Solve();
+
+  // TODO: FIXME (one or the other)
+  // h2oaikmeans::makePtr_dense<float>(n_gpu, rows, cols, 'r', k, &data[0], &res);
+  // make_ptr_float_kmeans(n_gpu, rows, cols, 'r', k, &data[0], &res);
 
   fflush(stdout);
   fflush(stderr);
