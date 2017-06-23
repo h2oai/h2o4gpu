6a284f1c6b19440dfbd509f3c0f36e030f7d1902
pseudotensor <pseudotensor@gmail.com>
2017-06-23 16:27:09 -0700

    centroids initially set correctly for multiple gpus
    Former-commit-id: e190125931a5473489899ea9e8e4e5d1080bc183
    Former-commit-id: 05fe92b2d103f26232a4bbb4a4862c715bfda619

--- src/gpu/h2oaikmeans.cu
+++ src/gpu/h2oaikmeans.cu
@@ -38,29 +38,29 @@ void random_data(thrust::device_vector<T>& array, int m, int n) {
 }
 
 template<typename T>
-void nonrandom_data(const char ord, thrust::device_vector<T>& array, const T *srcdata, int q, int n, int d) {
-  thrust::host_vector<T> host_array(n*d);
+void nonrandom_data(const char ord, thrust::device_vector<T>& array, const T *srcdata, int q, int n, int npergpu, int d) {
+  thrust::host_vector<T> host_array(npergpu*d);
   if(ord=='c'){
     fprintf(stderr,"COL ORDER -> ROW ORDER\n"); fflush(stderr);
     int indexi,indexj;
-    for(int i = 0; i < n * d; i++) {
+    for(int i = 0; i < npergpu * d; i++) {
 #if(1)
-      indexi = i%d;
-      indexj = i/d;
+      indexi = i%d; // col
+      indexj = i/d +  q*npergpu; // row (shifted by which gpu)
       //      host_array[i] = srcdata[indexi*n + indexj];
-            host_array[i] = srcdata[indexi*n + indexj];
+      host_array[i] = srcdata[indexi*n + indexj];
 #else
       indexj = i%d;
       indexi = i/d;
       //      host_array[i] = srcdata[indexi*n + indexj];
-            host_array[i] = srcdata[indexi*d + indexj];
+      host_array[i] = srcdata[indexi*d + indexj];
 #endif
     }
   }
   else{
     fprintf(stderr,"ROW ORDER not changed\n"); fflush(stderr);
-    for(int i = 0; i < n * d; i++) {
-      host_array[i] = srcdata[i];
+    for(int i = 0; i < npergpu * d; i++) {
+      host_array[i] = srcdata[q*npergpu*d + i]; // shift by which gpu
     }
   }
   array = host_array;
@@ -73,29 +73,52 @@ void random_labels(thrust::device_vector<int>& labels, int n, int k) {
   }
   labels = host_labels;
 }
-void nonrandom_labels(thrust::device_vector<int>& labels, const int *srclabels, int q, int n) {
-  thrust::host_vector<int> host_labels(n);
-  for(int i = 0; i < n; i++) {
-    host_labels[i] = srclabels[i];  //rand() % k;
+void nonrandom_labels(const char ord, thrust::device_vector<int>& labels, const int *srclabels, int q, int n, int npergpu) {
+  thrust::host_vector<int> host_labels(npergpu);
+  int d=1; // only 1 dimension
+  if(ord=='c'){
+    fprintf(stderr,"labels COL ORDER -> ROW ORDER\n"); fflush(stderr);
+    int indexi,indexj;
+    for(int i = 0; i < npergpu; i++) {
+#if(1)
+      indexi = i%d; // col
+      indexj = i/d +  q*npergpu; // row (shifted by which gpu)
+      //      host_labels[i] = srclabels[indexi*n + indexj];
+      host_labels[i] = srclabels[indexi*n + indexj];
+#else
+      indexj = i%d;
+      indexi = i/d;
+      //      host_labels[i] = srclabels[indexi*n + indexj];
+      host_labels[i] = srclabels[indexi*d + indexj];
+#endif
+    }
+  }
+  else{
+    fprintf(stderr,"labels ROW ORDER not changed\n"); fflush(stderr);
+    for(int i = 0; i < npergpu; i++) {
+      host_labels[i] = srclabels[q*npergpu*d + i]; // shift by which gpu
+    }
   }
   labels = host_labels;
 }
+
 template<typename T>
-void nonrandom_centroids(const char ord, thrust::device_vector<T>& array, const T *srcdata, int q, int n, int d, int k) {
+void random_centroids(const char ord, thrust::device_vector<T>& array, const T *srcdata, int q, int n, int npergpu, int d, int k) {
   thrust::host_vector<T> host_array(k*d);
   std::random_device rd;  //Will be used to obtain a seed for the random number engine
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<>dis(0, n-1); // random i in range from 0..n-1
+  //  std::uniform_int_distribution<>dis(0, npergpu-1); // random i in range from 0..npergpu-1
+    std::uniform_int_distribution<>dis(0, n-1); // random i in range from 0..n-1 (i.e. only 1 gpu gets centroids)
   if(ord=='c'){
     if(VERBOSE){
       fprintf(stderr,"COL ORDER -> ROW ORDER\n"); fflush(stderr);
     }
-    for(int i = 0; i < k; i++) {
-      int reali = dis(gen);
-      for(int j = 0; j < d; j++) {
+    for(int i = 0; i < k; i++) { // rows
+      int reali = dis(gen); // + q*npergpu; // row sampled (called indexj above)
+      for(int j = 0; j < d; j++) { // cols
         host_array[i*d+j] = srcdata[reali + j*n];
 #if(DEBUG)
-        fprintf(stderr,"initcent[%d,%d]=%g\n",i,j,host_array[i*d+j]); fflush(stderr);
+        fprintf(stderr,"q=%d initcent[%d,%d reali=%d]=%g\n",q,i,j,reali,host_array[i*d+j]); fflush(stderr);
 #endif
       }
     }
@@ -104,9 +127,9 @@ void nonrandom_centroids(const char ord, thrust::device_vector<T>& array, const 
     if(VERBOSE){
       fprintf(stderr,"ROW ORDER not changed\n"); fflush(stderr);
     }
-    for(int i = 0; i < k; i++) {
-      int reali = dis(gen);
-      for(int j = 0; j < d; j++) {
+    for(int i = 0; i < k; i++) { // rows
+      int reali = dis(gen); // + q*npergpu ; // row sampled
+      for(int j = 0; j < d; j++) { // cols
         host_array[i*d+j] = srcdata[reali*d + j];
       }
     }
@@ -224,7 +247,6 @@ namespace h2oaikmeans {
       std::cout << "Max. number of iterations: " << max_iterations << std::endl;
       std::cout << "Stopping threshold: " << threshold << std::endl;
 
-      //      bool init_from_labels=false;
       for (int q = 0; q < n_gpu; q++) {
         CUDACHECK(cudaSetDevice(q));
         std::cout << "Copying data to device: " << q << std::endl;
@@ -233,10 +255,39 @@ namespace h2oaikmeans {
         //        std::vector<T> vdata(&srcdata[q*n/n_gpu*d],&srcdata[(q+1)*n/n_gpu*d]);
         //        thrust::copy(vdata.begin(),vdata.end(),data[q]->begin());
         //random_labels(*labels[q], n/n_gpu, k);
-        nonrandom_data(ord,*data[q], &srcdata[q*n/n_gpu*d], q, n/n_gpu, d);
-        nonrandom_labels(*labels[q], &srclabels[q*n/n_gpu], q, n/n_gpu);
-        nonrandom_centroids(ord,*centroids[q], &srcdata[q*n/n_gpu*d], q, n/n_gpu, d, k);
+        nonrandom_data(ord, *data[q], &srcdata[0], q, n, n/n_gpu, d);
+        nonrandom_labels(ord, *labels[q], &srclabels[0], q, n, n/n_gpu);
       }
+      // get non-random centroids on 1 gpu, then share with rest.
+      if(init_from_labels==0){
+        int master_device=0;
+        int q=master_device;
+        CUDACHECK(cudaSetDevice(q));
+        random_centroids(ord, *centroids[q], &srcdata[0], q, n, n/n_gpu, d, k);
+        size_t bytecount = d*k; // all centroids
+
+        // copy centroids to rest of gpus asynchronously
+        std::vector<cudaStream_t *> streams;
+        streams.resize(n_gpu);
+        for (int q = 1; q < n_gpu; q++) {
+          CUDACHECK(cudaSetDevice(q));
+          std::cout << "Copying centroid data to device: " << q << std::endl;
+
+          streams[q] = reinterpret_cast<cudaStream_t*>(malloc(sizeof(cudaStream_t)));
+          cudaStreamCreate(streams[q]);
+          cudaMemcpyPeerAsync(thrust::raw_pointer_cast(&(*centroids[q])[0]),q,thrust::raw_pointer_cast(&(*centroids[master_device])[0]),master_device,bytecount,*(streams[q]));
+        }
+        for (int q = 1; q < n_gpu; q++) {
+          cudaSetDevice(q);
+          cudaStreamSynchronize(*(streams[q]));
+        }
+        for (int q = 1; q < n_gpu; q++) {
+          cudaSetDevice(q);
+          cudaStreamDestroy(*(streams[q]));
+        }
+      }
+      
+      
       double timetransfer = static_cast<double>(timer<double>() - t0t);
 
       double t0 = timer<double>();
