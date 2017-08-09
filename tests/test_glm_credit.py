6bbb73e03fb9e9a594014e1eb9bde3d94c948a92
navdeep-G <mr.navdeepgill@gmail.com>
2017-08-09 15:23:11 -0700

    Update logistic test
    Former-commit-id: 9e315212f53d225ca6482d97869205c27d8eb15f
    Former-commit-id: 15ccc6ad24413b06d822bd1a81fe09f936fcc7f1

--- tests/test_glm_credit.py
+++ tests/test_glm_credit.py
@@ -37,26 +37,36 @@ def fun(nGPUs=1, nFolds=1, nLambdas=100, nAlphas=8, validFraction=0.2, verbose=0
     print(logloss_train[0, 2])
     print(logloss_test[0, 2])
     sys.stdout.flush()
-    
-    if validFraction==0.0:
+
+    #Always checking the first 3 alphas with specific logloss scores (.48,.44)
+    if validFraction==0.0 and nFolds > 0:
         assert logloss_train[0, 0] < .48
-        assert logloss_train[0, 1] < .48
-        assert logloss_train[0, 2] < .48
-        assert logloss_test[0, 2] < .48
-            
+        assert logloss_train[0, 1] < .44
+        assert logloss_train[1, 0] < .48
+        assert logloss_test[1, 1] < .44
+        assert logloss_train[2, 0] < .48
+        assert logloss_test[2, 1] < .44
+    if validFraction > 0.0:
+        assert logloss_train[0, 2] < .44
+        assert logloss_train[1, 2] < .44
+        assert logloss_train[2, 2] < .44
+        assert logloss_test[0, 2] < .44
+        assert logloss_test[1, 2] < .44
+        assert logloss_test[2, 2] < .44
+
     print('/n Total execution time:%d' % (time.time() - t1))
 
     print("TEST PASSED")
     sys.stdout.flush()
 
     print("Time taken: {}".format(time.time() - t))
-    #    endfunnel(pipes)
+
     print("DONE.")
     sys.stdout.flush()
 
 
-def test_glm_credit_gpu_fold5_quick_train(): fun(nGPUs=1, nFolds=5, nLambdas=5, nAlphas=3, validFraction=0.0,verbose=0,family="logistic",print_all_errors=True)
-def test_glm_credit_gpu_fold5_quick_valid(): fun(nGPUs=1, nFolds=5, nLambdas=5, nAlphas=3, validFraction=0.2,verbose=0,family="logistic",print_all_errors=True)
+def test_glm_credit_gpu_fold5_quick_train(): fun(nGPUs=1, nFolds=5, nLambdas=5, nAlphas=3, validFraction=0.0,verbose=0,family="logistic",print_all_errors=False)
+def test_glm_credit_gpu_fold5_quick_valid(): fun(nGPUs=1, nFolds=5, nLambdas=5, nAlphas=3, validFraction=0.2,verbose=0,family="logistic",print_all_errors=False)
 
 
 if __name__ == '__main__':
