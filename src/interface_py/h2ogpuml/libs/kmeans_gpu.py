d1a922327247a79569ac9300e0fd9a4877b401ba
mdymczyk <dymczyk@gmail.com>
2017-08-11 14:49:09 -0700

    * Adds Python binding for kmeans transform method * Refactor kmeans C++ lib Python binding
    Former-commit-id: fd1c84b9f5ea4c7a679e8c5ea5315d489993bc1b
    Former-commit-id: 579eae703644150c3ffff8e7100ca39a4225791b

--- src/interface_py/h2ogpuml/libs/kmeans_gpu.py
+++ src/interface_py/h2ogpuml/libs/kmeans_gpu.py
@@ -1,26 +1,10 @@
-from ctypes import *
-from h2ogpuml.types import *
-
-
 class GPUlib():
     def __init__(self):
         pass
 
-    def get(self):
+    @staticmethod
+    def get():
         from h2ogpuml.libs.utils import gpu_lib_path
-        lib_path = gpu_lib_path()
+        from h2ogpuml.libs.utils import load_kmeans_lib
 
-        try:
-            h2ogpumlKMeansGPU = cdll.LoadLibrary(lib_path)
-
-            h2ogpumlKMeansGPU.make_ptr_float_kmeans.argtypes = [c_int, c_int, c_int, c_int, c_int, c_size_t, c_size_t, c_int, c_int, c_int, c_int,
-                                                                c_int, c_int, c_float, c_float_p, c_int_p, c_float_p, c_void_pp]
-            h2ogpumlKMeansGPU.make_ptr_float_kmeans.restype = c_int
-
-            h2ogpumlKMeansGPU.make_ptr_double_kmeans.argtypes = [c_int, c_int, c_int, c_int, c_int, c_size_t, c_size_t, c_int, c_int, c_int, c_int,
-                                                                 c_int, c_int, c_double, c_double_p, c_int_p, c_double_p, c_void_pp]
-            h2ogpumlKMeansGPU.make_ptr_double_kmeans.restype = c_int
-        except:
-            print('\nWarning: h2ogpumlKMeansGPU shared object (dynamic library) ' + lib_path + ' failed to load.')
-            h2ogpumlKMeansGPU = None
-        return h2ogpumlKMeansGPU
+        return load_kmeans_lib(gpu_lib_path())
