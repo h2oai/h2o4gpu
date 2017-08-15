18b2fee3b4cc097e9198d583e7822553d781f909
pseudotensor <pseudotensor@gmail.com>
2017-08-15 13:17:03 -0700

    show results when doing perf tests
    Former-commit-id: 0d404a319e3303e00ed8bd0fa88b18374a99b6b3
    Former-commit-id: 07e0da448f6813e38bfe00e8966879663809b519

---
+++ showresults.sh
@@ -0,0 +1,7 @@
+path="results"
+list=`ls $path | sort`
+#for fil in $list ; do echo $fil ; done
+rm -rf resultsall.dat
+echo "Run Train CV Valid ..." >> resultsall.dat
+for fil in $list ; do cat $path/$fil >> resultsall.dat ; done
+column -t resultsall.dat
