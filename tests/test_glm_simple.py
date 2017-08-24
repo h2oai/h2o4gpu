be449277a8f800dee1a773a20d5dfa8cd7bd890c
pseudotensor <pseudotensor@gmail.com>
2017-08-23 17:43:38 -0700

    minor asserts due to fold number change.

--- tests/test_glm_simple.py
+++ tests/test_glm_simple.py
@@ -67,7 +67,7 @@ def fun(nGPUs=1, nFolds=1, nLambdas=100, nAlphas=8, validFraction=0.2):
                 assert rmse_train[0, 2] < 0.4
                 assert rmse_test[0, 2] < 0.4
 
-                assert rmse_train[-1, 0] < 0.31
+                assert rmse_train[-1, 0] < 0.37
                 assert rmse_train[-1, 1] < 0.42
                 assert rmse_train[-1, 2] < 0.4
                 assert rmse_test[-1, 2] < 0.4
@@ -88,7 +88,7 @@ def fun(nGPUs=1, nFolds=1, nLambdas=100, nAlphas=8, validFraction=0.2):
                 assert rmse_train[0, 2] < 0.4
                 assert rmse_test[0, 2] < 0.4
 
-                assert rmse_train[-1, 0] < 0.31
+                assert rmse_train[-1, 0] < 0.37
                 assert rmse_train[-1, 1] < 0.24
                 assert rmse_train[-1, 2] < 0.4
                 assert rmse_test[-1, 2] < 0.4
