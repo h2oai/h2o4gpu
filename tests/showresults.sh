bf5f9ab284aff16545097005d511517059255e19
pseudotensor <pseudotensor@gmail.com>
2017-08-23 17:34:04 -0700

    minor

--- tests/showresults.sh
+++ tests/showresults.sh
@@ -9,6 +9,8 @@ echo "RunErrorRel Train CV Valid ..." >> resultsall.error.dat
 for fil in $list ; do cat $fil >> resultsall.error.dat ; done
 column -t resultsall.error.dat
 
+echo "" 
+
 ############## H2O ERRORS
 list=`ls $path/*.error.h2o.dat | sort`
 #for fil in $list ; do echo $fil ; done
@@ -16,6 +18,8 @@ rm -rf resultsall.error.h2o.dat
 echo "RunErrorH2O Train CV Valid ..." >> resultsall.error.h2o.dat
 for fil in $list ; do cat $fil >> resultsall.error.h2o.dat ; done
 column -t resultsall.error.h2o.dat
+
+echo "" 
 
 ############## H2O4GPU ERRORS
 list=`ls $path/*.error.h2o4gpu.dat | sort`
@@ -25,6 +29,8 @@ echo "RunErrorH2O4GPU Train CV Valid ..." >> resultsall.error.h2o4gpu.dat
 for fil in $list ; do cat $fil >> resultsall.error.h2o4gpu.dat ; done
 column -t resultsall.error.h2o4gpu.dat
 
+echo "" 
+
 ############## RELATIVE TIMES
 list=`ls $path/*.time.dat | sort`
 #for fil in $list ; do echo $fil ; done
@@ -32,6 +38,8 @@ rm -rf resultsall.time.dat
 echo "RunTimeRatio Train CV Valid ..." >> resultsall.time.dat
 for fil in $list ; do cat $fil >> resultsall.time.dat ; done
 column -t resultsall.time.dat
+
+echo "" 
 
 ############## H2O TIMES
 list=`ls $path/*.time.h2o.dat | sort`
@@ -41,6 +49,8 @@ echo "RunTimeH2O Train CV Valid ..." >> resultsall.time.h2o.dat
 for fil in $list ; do cat $fil >> resultsall.time.h2o.dat ; done
 column -t resultsall.time.h2o.dat
 
+echo "" 
+
 ############## H2O4GPU TIMES
 list=`ls $path/*.time.h2o4gpu.dat | sort`
 #for fil in $list ; do echo $fil ; done
@@ -48,3 +58,5 @@ rm -rf resultsall.time.h2o4gpu.dat
 echo "RunTimeH2O4GPU Train CV Valid ..." >> resultsall.time.h2o4gpu.dat
 for fil in $list ; do cat $fil >> resultsall.time.h2o4gpu.dat ; done
 column -t resultsall.time.h2o4gpu.dat
+
+echo "" 
