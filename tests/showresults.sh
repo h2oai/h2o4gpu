cc8a1fa9846fd2803a552e3e221fc771bc73d349
pseudotensor <pseudotensor@gmail.com>
2017-08-24 09:43:45 -0700

    showresults updates

--- tests/showresults.sh
+++ tests/showresults.sh
@@ -4,59 +4,59 @@ path="results"
 ############## RELATIVE ERRORS
 list=`ls $path/*.error.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.error.dat
-echo "RunErrorRel Train CV Valid ..." >> resultsall.error.dat
-for fil in $list ; do cat $fil >> resultsall.error.dat ; done
-column -t resultsall.error.dat
+rm -rf $path/results.error.all.dat
+echo "RunErrorRel Train CV Valid ..." >> $path/results.error.all.dat
+for fil in $list ; do cat $fil >> $path/results.error.all.dat ; done
+column -t $path/results.error.all.dat
 
 echo "" 
 
 ############## H2O ERRORS
 list=`ls $path/*.error.h2o.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.error.h2o.dat
-echo "RunErrorH2O Train CV Valid ..." >> resultsall.error.h2o.dat
-for fil in $list ; do cat $fil >> resultsall.error.h2o.dat ; done
-column -t resultsall.error.h2o.dat
+rm -rf $path/results.error.h2o.all.dat
+echo "RunErrorH2O Train CV Valid ..." >> $path/results.error.h2o.all.dat
+for fil in $list ; do cat $fil >> $path/results.error.h2o.all.dat ; done
+column -t $path/results.error.h2o.all.dat
 
 echo "" 
 
 ############## H2O4GPU ERRORS
 list=`ls $path/*.error.h2o4gpu.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.error.h2o4gpu.dat
-echo "RunErrorH2O4GPU Train CV Valid ..." >> resultsall.error.h2o4gpu.dat
-for fil in $list ; do cat $fil >> resultsall.error.h2o4gpu.dat ; done
-column -t resultsall.error.h2o4gpu.dat
+rm -rf $path/results.error.h2o4gpu.all.dat
+echo "RunErrorH2O4GPU Train CV Valid ..." >> $path/results.error.h2o4gpu.all.dat
+for fil in $list ; do cat $fil >> $path/results.error.h2o4gpu.all.dat ; done
+column -t $path/results.error.h2o4gpu.all.dat
 
 echo "" 
 
 ############## RELATIVE TIMES
 list=`ls $path/*.time.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.time.dat
-echo "RunTimeRatio Train CV Valid ..." >> resultsall.time.dat
-for fil in $list ; do cat $fil >> resultsall.time.dat ; done
-column -t resultsall.time.dat
+rm -rf $path/results.time.all.dat
+echo "RunTimeRatio Time_Ratio" >> $path/results.time.all.dat
+for fil in $list ; do cat $fil >> $path/results.time.all.dat ; done
+column -t $path/results.time.all.dat
 
 echo "" 
 
 ############## H2O TIMES
 list=`ls $path/*.time.h2o.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.time.h2o.dat
-echo "RunTimeH2O Train CV Valid ..." >> resultsall.time.h2o.dat
-for fil in $list ; do cat $fil >> resultsall.time.h2o.dat ; done
-column -t resultsall.time.h2o.dat
+rm -rf $path/results.time.h2o.all.dat
+echo "RunTimeH2O Time_H2O" >> $path/results.time.h2o.all.dat
+for fil in $list ; do cat $fil >> $path/results.time.h2o.all.dat ; done
+column -t $path/results.time.h2o.all.dat
 
 echo "" 
 
 ############## H2O4GPU TIMES
 list=`ls $path/*.time.h2o4gpu.dat | sort`
 #for fil in $list ; do echo $fil ; done
-rm -rf resultsall.time.h2o4gpu.dat
-echo "RunTimeH2O4GPU Train CV Valid ..." >> resultsall.time.h2o4gpu.dat
-for fil in $list ; do cat $fil >> resultsall.time.h2o4gpu.dat ; done
-column -t resultsall.time.h2o4gpu.dat
+rm -rf $path/results.time.h2o4gpu.all.dat
+echo "RunTimeH2O4GPU Time_H2O4GPU" >> $path/results.time.h2o4gpu.all.dat
+for fil in $list ; do cat $fil >> $path/results.time.h2o4gpu.all.dat ; done
+column -t $path/results.time.h2o4gpu.all.dat
 
 echo "" 
