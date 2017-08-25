50c24126a5936a57ad75a347a2ee13abf6252f61
navdeep-G <mr.navdeepgill@gmail.com>
2017-08-25 10:42:53 -0700

    Update init files

--- src/interface_py/h2o4gpu/solvers/__init__.py
+++ src/interface_py/h2o4gpu/solvers/__init__.py
@@ -2,6 +2,6 @@ from h2o4gpu.solvers.base import Pogs
 from h2o4gpu.solvers.elastic_net_base import GLM
 from h2o4gpu.solvers.logistic import LogisticRegression
 from h2o4gpu.solvers.elastic_net_base import LinearRegression
-from h2o4gpu.solvers.elastic_net_base import Lasso
+from h2o4gpu.solvers.lasso import Lasso
 from h2o4gpu.solvers.elastic_net_base import Ridge
 from h2o4gpu.solvers.kmeans_base import KMeans
