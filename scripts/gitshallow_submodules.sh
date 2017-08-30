afc5c705677a15c15fff84c4a77b5e4a6e320a41
pseudotensor <pseudotensor@gmail.com>
2017-08-30 17:37:34 -0400

    update scikit-learn repo and module

--- scripts/gitshallow_submodules.sh
+++ scripts/gitshallow_submodules.sh
@@ -6,8 +6,8 @@ for i in $(git submodule | awk '{print $2}'); do
     surl=$(git config -f .gitmodules --get submodule.$i.url)
     echo "submodule:" $i $spath $surl
 #    if [ $spath == "cub" ] || [ $spath == "nccl" ] || [ $spath == "py3nvml" ] || [ $spath == "scikit-learn || [ $spath == "xgboost" ] ; then
-#    if [ $spath == "cub" ] || [ $spath == "nccl" ] || [ $spath == "py3nvml" ] || [ $spath == "scikit-learn" ] ; then # can't add xgboost because not pulling from master
-    if [ $spath == "cub" ] || [ $spath == "nccl" ] || [ $spath == "py3nvml" ] ; then # can't add xgboost because not pulling from master
+    if [ $spath == "cub" ] || [ $spath == "nccl" ] || [ $spath == "py3nvml" ] || [ $spath == "scikit-learn" ] ; then # can't add xgboost because not pulling from master
+#    if [ $spath == "cub" ] || [ $spath == "nccl" ] || [ $spath == "py3nvml" ] ; then # can't add xgboost because not pulling from master
         git submodule update --depth 1 $spath
     else
         git submodule update $spath
