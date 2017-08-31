afcce82791f8159488fef197066e6a5edf947b3b
Navdeep Gill <mr.navdeepgill@gmail.com>
2017-08-30 17:58:32 -0700

    Add more metrics to util/metrics.py (#91)
    * Add more evaluation metrics
    
    * Add proper documentation for metrics.py. Clean up code a bit

--- src/interface_py/h2o4gpu/util/metrics.py
+++ src/interface_py/h2o4gpu/util/metrics.py
@@ -5,22 +5,20 @@
 """
 import numpy as np
 
-
 def ll(actual, predicted):
     """
     Computes the log likelihood.
+
     This function computes the log likelihood between two numbers,
     or for element between a pair of lists or numpy arrays.
-    Parameters
-    ----------
-    actual : int, float, list of numbers, numpy array
-             The ground truth value
-    predicted : same type as actual
-                The predicted value
-    Returns
-    -------
-    score : double or list of doubles
-            The log likelihood error between actual and predicted
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double or list of doubles
+             The log likelihood error between actual and predicted
     """
     actual = np.array(actual)
     predicted = np.array(predicted)
@@ -37,78 +35,225 @@ def ll(actual, predicted):
             score = 0
     return score
 
-
 def log_loss(actual, predicted):
     """
     Computes the log loss.
+
     This function computes the log loss between two lists
     of numbers.
-    Parameters
-    ----------
-    actual : list of numbers, numpy array
-             The ground truth value
-    predicted : same type as actual
-                The predicted value
-    Returns
-    -------
-    score : double
-            The log loss between actual and predicted
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+             The log loss between actual and predicted
     """
     return np.mean(ll(actual, predicted))
-
 
 def se(actual, predicted):
     """
     Computes the squared error.
+
     This function computes the squared error between two numbers,
     or for element between a pair of lists or numpy arrays.
-    Parameters
-    ----------
-    actual : int, float, list of numbers, numpy array
-             The ground truth value
-    predicted : same type as actual
-                The predicted value
-    Returns
-    -------
-    score : double or list of doubles
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double or list of doubles
             The squared error between actual and predicted
     """
     return np.power(np.array(actual) - np.array(predicted), 2)
 
-
 def mse(actual, predicted):
     """
     Computes the mean squared error.
+
     This function computes the mean squared error between two lists
     of numbers.
-    Parameters
-    ----------
-    actual : list of numbers, numpy array
-             The ground truth value
-    predicted : same type as actual
-                The predicted value
-    Returns
-    -------
-    score : double
-            The mean squared error between actual and predicted
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+             The mean squared error between actual and predicted
     """
     return np.mean(se(actual, predicted))
-
 
 def rmse(actual, predicted):
     """
     Computes the root mean squared error.
+
     This function computes the root mean squared error between two lists
     of numbers.
-    Parameters
-    ----------
-    actual : list of numbers, numpy array
-             The ground truth value
-    predicted : same type as actual
-                The predicted value
-    Returns
-    -------
-    score : double
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
             The root mean squared error between actual and predicted
     """
     return np.sqrt(mse(actual, predicted))
+
+def ce(actual, predicted):
+    """
+    Computes the classification error.
+
+    This function computes the classification error between two lists
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+            The classification error between actual and predicted
+    """
+    return (sum([1.0 for x, y in zip(actual, predicted) if x != y]) /
+            len(actual))
+
+def ae(actual, predicted):
+    """
+    Computes the absolute error.
+
+    This function computes the absolute error between two numbers,
+    or for element between a pair of lists or numpy arrays.
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double or list of doubles
+            The absolute error between actual and predicted
+    """
+    return np.abs(np.array(actual)-np.array(predicted))
+
+def mae(actual, predicted):
+    """
+    Computes the mean absolute error.
+
+    This function computes the mean absolute error between two lists
+    of numbers.
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+            The mean absolute error between actual and predicted
+    """
+    return np.mean(ae(actual, predicted))
+
+def sle(actual, predicted):
+    """
+    Computes the squared log error.
+
+    This function computes the squared log error between two numbers,
+    or for element between a pair of lists or numpy arrays.
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double or list of doubles
+             The squared log error between actual and predicted
+    """
+    return (np.power(np.log(np.array(actual)+1) -
+                     np.log(np.array(predicted)+1), 2))
+
+def msle(actual, predicted):
+    """
+    Computes the mean squared log error.
+
+    This function computes the mean squared log error between two lists
+    of numbers.
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+            The mean squared log error between actual and predicted
+    """
+    return np.mean(sle(actual, predicted))
+
+def rmsle(actual, predicted):
+    """
+    Computes the root mean squared log error.
+
+    This function computes the root mean squared log error between two lists
+    of numbers.
+
+    :param actual : int, float, list of numbers, numpy array
+                    The ground truth value
+    :param predicted : same type as actual
+                     The predicted value
+
+    :returns double
+            The root mean squared log error between actual and predicted
+    """
+    return np.sqrt(msle(actual, predicted))
+
+def tied_rank(x):
+    """
+    Computes the tied rank of elements in x.
+
+    This function computes the tied rank of elements in x.
+
+    :param x : list of numbers, numpy array
+
+    :returns list of numbers
+            The tied rank f each element in x
+    """
+    sorted_x = sorted(zip(x, range(len(x))))
+    r = [0] * len(x)
+    cur_val = sorted_x[0][0]
+    last_rank = 0
+    for i in range(len(sorted_x)):
+        if cur_val != sorted_x[i][0]:
+            cur_val = sorted_x[i][0]
+            for j in range(last_rank, i):
+                r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
+            last_rank = i
+        if i == len(sorted_x)-1:
+            for j in range(last_rank, i+1):
+                r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
+    return r
+
+def auc(actual, posterior):
+    """
+    Computes the area under the receiver-operater characteristic (AUC)
+
+    This function computes the AUC error metric for binary classification.
+
+    :param actual : list of binary numbers, numpy array
+                    The ground truth value
+    :param posterior : same type as actual
+                       Defines a ranking on the binary numbers,
+                       from most likely to be positive to least
+                       likely to be positive.
+
+    :returns double
+             The AUC between actual and posterior
+    """
+    r = tied_rank(posterior)
+    num_positive = len([0 for x in actual if x == 1])
+    num_negative = len(actual)-num_positive
+    sum_positive = sum([r[i] for i in range(len(r)) if actual[i] == 1])
+    area_under_curve = ((sum_positive - num_positive*(num_positive+1)/2.0) /
+                        (num_negative*num_positive))
+    return area_under_curve
+
+
