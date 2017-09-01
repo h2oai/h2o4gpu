601823d839db16df67f4f760f3b684984186983f
mdymczyk <dymczyk@gmail.com>
2017-09-01 10:49:05 +0900

    Kmeans cleanup
    * Group files together
    * Remove duplicate code
    * Add a v. simple logger

---
+++ src/common/logger.h
@@ -0,0 +1,27 @@
+/* Copyright 2017 H2O.ai
+
+Apache License Version 2.0 (see LICENSE for details)
+==============================================================================*/
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define H2O4GPU_LOG_NOTHING    0   // Fatals are errors terminating the program immediately
+#define H2O4GPU_LOG_FATAL    100   // Fatals are errors terminating the program immediately
+#define H2O4GPU_LOG_ERROR    200   // Errors are when the program may not exit
+#define H2O4GPU_LOG_INFO     300   // Info
+#define H2O4GPU_LOG_WARN     400   // Warns about unwanted, but not dangerous, state/behaviour
+#define H2O4GPU_LOG_DEBUG    500   // Most basic debug information
+#define H2O4GPU_LOG_VERBOSE  600   // Everything possible
+
+void log_fatal(const int verbosity, const char* message);
+void log_error(const int verbosity, const char* message);
+void log_info(const int verbosity, const char* message);
+void log_warn(const int verbosity, const char* message);
+void log_debug(const int verbosity, const char* message);
+void log_verbose(const int verbosity, const char* message);
+
+#ifdef __cplusplus
+}
+#endif
--- src/gpu/kmeans/kmeans_general.h
+++ src/gpu/kmeans/kmeans_general.h
@@ -2,9 +2,8 @@
 
 Apache License Version 2.0 (see LICENSE for details)
 ==============================================================================*/
-
-#ifndef __KMEANS_GENERAL_H
-#define  __KMEANS_GENERAL_H
+#pragma once
+#include "../../common/logger.h"
 #define MAX_NGPUS 16
 
 #define VERBOSE 0
@@ -12,7 +11,6 @@ Apache License Version 2.0 (see LICENSE for details)
 #define DEBUGKMEANS 0
 
 // TODO(pseudotensor): Avoid throw for python exception handling.  Need to avoid all exit's and return exit code all the way back.
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+#define gpuErrchk(ans) { gpu_assert((ans), __FILE__, __LINE__); }
 #define safe_cuda(ans) throw_on_cuda_error((ans), __FILE__, __LINE__);
 #define safe_cublas(ans) throw_on_cublas_error((ans), __FILE__, __LINE__);
-#endif
