993af6bd32b08961bb965ca7387409a452bdfc0f
mdymczyk <dymczyk@gmail.com>
2017-09-01 10:49:05 +0900

    Kmeans cleanup
    * Group files together
    * Remove duplicate code
    * Add a v. simple logger

--- src/include/h2o4gpukmeans.h
+++ src/include/h2o4gpukmeans.h
@@ -10,49 +10,49 @@
 
 namespace h2o4gpukmeans {
 
-    static const std::string H2O4GPUKMEANS_VERSION = "0.0.1";
+static const std::string H2O4GPUKMEANS_VERSION = "0.0.3";
 
-    template<typename M>
-    class H2O4GPUKMeans {
-    private:
-        // Data
-        const M *_A;
-        int _k;
-        int _n;
-        int _d;
-    public:
-        H2O4GPUKMeans(const M *A, int k, int n, int d);
-    };
+template<typename M>
+class H2O4GPUKMeans {
+ private:
+  // Data
+  const M *_A;
+  int _k;
+  int _n;
+  int _d;
+ public:
+  H2O4GPUKMeans(const M *A, int k, int n, int d);
+};
 
-    template<typename M>
-    class H2O4GPUKMeansCPU {
-    private:
-        // Data
-        const M *_A;
-        int _k;
-        int _n;
-        int _d;
-    public:
-        H2O4GPUKMeansCPU(const M *A, int k, int n, int d);
-    };
+template<typename M>
+class H2O4GPUKMeansCPU {
+ private:
+  // Data
+  const M *_A;
+  int _k;
+  int _n;
+  int _d;
+ public:
+  H2O4GPUKMeansCPU(const M *A, int k, int n, int d);
+};
 
-    template<typename T>
-    int makePtr_dense(int dopredict, int verbose, int seed, int gpu_id, int n_gpu, size_t rows, size_t cols,
-                      const char ord, int k, int max_iterations, int init_from_data, int init_data,
-                      T threshold, const T *srcdata, const int *srclabels, const T *centroids,
+template<typename T>
+int makePtr_dense(int dopredict, int verbose, int seed, int gpu_id, int n_gpu, size_t rows, size_t cols,
+                  const char ord, int k, int max_iterations, int init_from_data, int init_data,
+                  T threshold, const T *srcdata, const int *srclabels, const T *centroids,
+                  void **pred_centroids, void **pred_labels);
+
+template<typename T>
+int makePtr_dense_cpu(int dopredict, int verbose, int seed, int cpu_id, int n_cpu, size_t rows, size_t cols,
+                      const char ord, int k, int max_iterations, int init_from_data,
+                      int init_data, T threshold, const T *srcdata, const int *srclabels, const T *centroids,
                       void **pred_centroids, void **pred_labels);
 
-    template<typename T>
-    int makePtr_dense_cpu(int dopredict, int verbose, int seed, int cpu_id, int n_cpu, size_t rows, size_t cols,
-                          const char ord, int k, int max_iterations, int init_from_data,
-                          int init_data, T threshold, const T *srcdata, const int *srclabels, const T *centroids,
-                          void **pred_centroids, void **pred_labels);
-
-    template<typename T>
-    int kmeans_transform(int verbose,
-                         int gpu_id, int n_gpu,
-                         size_t m, size_t n, const char ord, int k,
-                         const T *srcdata, const T *centroids,
-                         void **preds);
+template<typename T>
+int kmeans_transform(int verbose,
+                     int gpu_id, int n_gpu,
+                     size_t m, size_t n, const char ord, int k,
+                     const T *srcdata, const T *centroids,
+                     void **preds);
 
 }  // namespace h2o4gpukmeans
