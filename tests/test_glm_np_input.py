c9bbc4ba82975d33c54d47b5b3b5d1317e993838
pseudotensor <pseudotensor@gmail.com>
2017-09-03 16:21:24 -0700

    minor assert

--- tests/test_glm_np_input.py
+++ tests/test_glm_np_input.py
@@ -19,15 +19,23 @@ def func():
 #
     print('Linear Regression')
     test0 = np.array([15.0])
-    print('Predicted:', lm.predict(test0))
-    print('Predicted:', lm.predict(np.array([15.0])))
+    result1 = lm.predict(test0)
+    result2 = lm.predict(np.array([15.0]))
     test = np.array([15.0]).astype(np.float32) #pass in data that is already float32
-    print('Predicted:', lm.predict(test))
+    result3 = lm.predict(test)
+
+    print('Predicted:', result1)
+    print('Predicted:', result2)
+    print('Predicted:', result3)
     print('Coefficients:', lm.X)
 #
     #Assert coefficients are within a reasonable range for various prediction values
-    assert lm.X[0][0] - a < 1e-3
-    assert lm.X[0][1] - b < 1e-3
+    assert np.fabs(lm.X[0][0] - a) < 1.1e-3
+    assert np.fabs(lm.X[0][1] - b) < 1.1e-3
+
+    assert np.fabs(result1 -  40.0) < 1.1e-3
+    assert np.fabs(result2 - 40.0) < 1.1e-3
+    assert np.fabs(result3 - 40.0) < 1.1e-3
 #
 #
 def test_glm_np_input(): func()
