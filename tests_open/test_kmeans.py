d9148374cc003999c62ca4d8b1a9799069794f0f
mdymczyk <dymczyk@gmail.com>
2017-09-08 08:48:41 +0900

    Move KMeans sklearn tests to tests_open

--- tests_open/test_kmeans.py
+++ tests_open/test_kmeans.py
@@ -1,121 +1,90 @@
 # -*- encoding: utf-8 -*-
 """
-KMeans solver tests using Kaggle datasets.
+KMeans solver tests using SKLearn datasets.
 
 :copyright: (c) 2017 H2O.ai
 :license:   Apache License Version 2.0 (see LICENSE for details)
 """
-import h2o4gpu as h2o4gpu
-import pandas as pd
+from h2o4gpu import KMeans
+from h2o4gpu.datasets import load_iris
 import numpy as np
 
 
 class TestKmeans(object):
-    def test_preds_vs_scikit(self):
-        X = self._fetch_data()
-        model = self._train_model(X)
+    def test_fit_iris(self):
+        X = load_iris().data
+        clusters = 4
+        model = KMeans(n_gpus=1, n_clusters=clusters, seed=123).fit(X)
 
-        train_labels = model.predict(X)
-        print(train_labels)
-        sklearn_labels = model.sklearn_predict(X)
-        print(sklearn_labels)
+        assert model.cluster_centers_.shape == (X.shape[1], clusters)
 
-        diffs = 0
-        for tl in zip(train_labels, sklearn_labels):
-            if tl[0] != tl[1]:
-                diffs = diffs + 1
+        model_rerun = KMeans(n_gpus=1, n_clusters=clusters, seed=123).fit(X)
 
-        assert diffs / X.shape[0] <= 0.1
+        # Same seed should yield same results
+        assert np.allclose(
+            model.cluster_centers_, model_rerun.cluster_centers_
+        )
 
-    def test_transform_vs_scikit(self):
-        X = self._fetch_data()
-        model = self._train_model(X)
+        model_rerun2 = model_rerun.fit(X)
 
-        h2o_labels = \
-            list(map(lambda x: np.argmin(x), model.transform(X)))
+        # Multiple invocations of fit with the same seed
+        # also should produce the same result
+        assert np.allclose(
+            model_rerun.cluster_centers_, model_rerun2.cluster_centers_
+        )
 
-        sklearn_labels = \
-            list(
-                map(lambda x: np.argmin(x),
-                    model.sklearn_transform(X))
-            )
+        # model_all = KMeans(n_clusters=clusters, seed=123).fit(X)
 
-        diffs = 0
-        for tl in zip(h2o_labels, sklearn_labels):
-            if tl[0] != tl[1]:
-                diffs = diffs + 1
+        # Multi GPU should yield same result as single GPU
+        # TODO multi GPU returns wrong results
+        # assert np.allclose(
+        #     model.cluster_centers_, model_all.cluster_centers_
+        # )
 
-        assert diffs / X.shape[0] <= 0.1
+    def test_fit_vs_sk_iris(self):
+        X = load_iris().data
+        model = KMeans(n_gpus=1, n_clusters=4, seed=1234).fit(X)
 
-    @staticmethod
-    def _train_model(trainencflt):
-        model = h2o4gpu.KMeans(n_gpus=1, n_clusters=10, tol=1e-7,
-                               max_iter=100, init_data='selectstrat')
-        return model.fit(trainencflt)
+        h2o_labels = model.predict(X)
+        sk_lables = model.sklearn_predict(X)
 
-    @staticmethod
-    def _fetch_data():
-        import pandas as pd
-        if 1==1:
-            from h2o4gpu.datasets import fetch_20newsgroups, fetch_20newsgroups_vectorized, fetch_california_housing, \
-                fetch_covtype, fetch_kddcup99, fetch_lfw_pairs, fetch_lfw_people, fetch_mldata, fetch_olivetti_faces, \
-                fetch_rcv1, fetch_species_distributions
-            from h2o4gpu.model_selection import train_test_split
-            data = fetch_covtype()
-            sizetokeep = 1000  # 1k rows for now
-            sizetokeep = min(sizetokeep, len(data.data[:, 0]))
-            X = data.data[0:sizetokeep, :]
-            y = data.target[0:sizetokeep]
-            print("Got Data")
+        assert all(h2o_labels == sk_lables)
 
-            # Create 0.8/0.2 train/test split
-            print("Split Data")
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8,
-                                                                random_state=42)
+    def test_fit_iris_precision(self):
+        X_f64 = load_iris().data
+        X_f32 = X_f64.astype(np.float32)
+        kmeans = KMeans(n_gpus=1, n_clusters=4, seed=12345)
+        model_f64_labels = kmeans.fit(X_f64).predict(X_f64)
+        model_f32_labels = kmeans.fit(X_f32).predict(X_f32)
 
-            test= pd.DataFrame(X_test)
-            train = pd.DataFrame(X_train)
+        assert all(model_f64_labels == model_f32_labels)
 
-        # https://github.com/caesar0301/awesome-public-datasets
-        else:
-            # TODO: FIXME
-            train_file = "./data/???_train.csv"
-            test_file = "./data/???_test.csv"
+    # On data where we don't loose precision, predictions should be the same
+    def test_fit_i32_vs_f32(self):
+        X_f64 = np.array([[1., 2.], [1., 4.], [1., 0.], [4., 2.], [4., 4.], [4., 0.]])
+        X_f32 = X_f64.astype(np.float32)
+        X_i32 = X_f64.astype(np.int32)
+        kmeans = KMeans(n_gpus=1, n_clusters=2, seed=123)
 
-            train = pd.read_csv(train_file)
-            test = pd.read_csv(test_file)
-            train.drop(['QuoteConversion_Flag'], axis=1, inplace=True)
+        model_f64_labels = kmeans.fit(X_f64).predict(X_f64)
+        model_f32_labels = kmeans.fit(X_f32).predict(X_f32)
+        model_i32_labels = kmeans.fit(X_i32).predict(X_i32)
 
-        dataset = pd.concat([train, test], ignore_index=True)
-        tmp = dataset.dtypes.reset_index().rename(
-            columns={0: "type"})  # ["index"]
-        indx = tmp["type"] == "object"
-        categoricals = tmp[indx]["index"].tolist()
-        # replace nans as new category
-        for col in dataset.columns:
-            dataset[col] = dataset[col].fillna("__NA__")
+        assert all(model_f64_labels == model_f32_labels)
+        assert all(model_f32_labels == model_i32_labels)
 
-        # encode unfreq categories
-        for col in categoricals:
-            val_dict = dataset[col].value_counts()
-            val_dict = dataset[col].value_counts().reset_index()
-            indx = val_dict[col] < 100
-            res = val_dict[indx]["index"].tolist()
-            indx = dataset[col].isin(res)
-            vals = dataset[col].values
-            vals[indx] = "___UNFREQ___"
-            dataset[col] = vals
+    def test_predict_iris(self):
+        X = load_iris().data
+        model = KMeans(n_gpus=1, n_clusters=4, seed=123456).fit(X)
 
-        # encode all as freqs
-        for col in categoricals:
-            val_dict = dataset[col].value_counts()
-            val_dict = val_dict / float(dataset.shape[0])
-            val_dict = val_dict.to_dict()
-            dataset[col] = dataset[col].apply(lambda x: val_dict[x])
+        assert all(model.labels_ == model.predict(X))
 
-        # replace nans as new category
-        for col in dataset.columns:
-            dataset[col] = dataset[col].replace("__NA__", 0)
-        trainenc = dataset.iloc[:train.shape[0], :].reset_index(drop=True)
-        trainencflt = trainenc.values.astype(np.float32)
-        return trainencflt
+    def test_transform_iris(self):
+        X = load_iris().data
+        model = KMeans(n_gpus=1, n_clusters=4, seed=1234567).fit(X)
+
+        labels_from_trans = list(
+            map(lambda x: np.argmin(x), model.transform(X))
+        )
+
+        assert all(labels_from_trans == model.predict(X))
