3d94948c4b42aaef9a6fa54a0d069be09b4069e0
navdeep-G <mr.navdeepgill@gmail.com>
2017-09-07 14:43:12 -0700

    Use open_data dir for now in tests_open

--- tests_open/test_logistic_credit.py
+++ tests_open/test_logistic_credit.py
@@ -22,7 +22,7 @@ def func(nGPUs=1, nFolds=1, nLambdas=100, nAlphas=8, validFraction=0.2, verbose=
     sys.stdout.flush()
 
     print("Reading Data")
-    df = pd.read_csv("./data/creditcard.csv")
+    df = pd.read_csv("./open_data/creditcard.csv")
     print(df.shape)
     X = np.array(df.iloc[:, :df.shape[1] - 1], dtype='float32', order='C')
     y = np.array(df.iloc[:, df.shape[1] - 1], dtype='float32', order='C')
