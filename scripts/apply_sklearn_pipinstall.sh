2d206d910e4e39fbb896b4042f263df774d88d03
pseudotensor <pseudotensor@gmail.com>
2017-09-08 17:44:05 -0700

    update for any python for sklearn

--- scripts/apply_sklearn_pipinstall.sh
+++ scripts/apply_sklearn_pipinstall.sh
@@ -3,5 +3,7 @@
 # apply sklearn
 rm -rf sklearn
 cd scikit-learn
-pip install dist/h2o4gpu-0.20.dev0-cp36-cp36m-linux_x86_64.whl --upgrade --target ../sklearn/
+file=`ls dist/h2o4gpu*.whl`
+
+pip install $file --upgrade --target ../sklearn/
 cd ../
