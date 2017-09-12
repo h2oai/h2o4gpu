ef6a905db57e82c7006217d838f9a810f3396b78
pseudotensor <pseudotensor@gmail.com>
2017-09-12 10:40:51 -0700

    clean up requirements for separate build and runtime versions

--- src/interface_py/setup.py
+++ src/interface_py/setup.py
@@ -67,7 +67,7 @@ class H2O4GPUInstall(install):
         self.copy_tree(self.build_lib, self.install_lib)
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('../../requirements.txt', session='hack')
+install_reqs = parse_requirements('requirements.txt', session='hack')
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
