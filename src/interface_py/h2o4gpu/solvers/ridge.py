aa25da482acc6a1dc9be0e5f63e5d845d7fa064d
navdeep-G <mr.navdeepgill@gmail.com>
2017-09-15 15:07:20 -0700

    Add sklearn options for Ridge

--- src/interface_py/h2o4gpu/solvers/ridge.py
+++ src/interface_py/h2o4gpu/solvers/ridge.py
@@ -1,101 +1,139 @@
-#- * - encoding : utf - 8 - * -
+# - * - encoding : utf - 8 - * -
 """
 :copyright: 2017 H2O.ai, Inc.
 :license:   Apache License Version 2.0 (see LICENSE for details)
 """
-from h2o4gpu.solvers.elastic_net import GLM
+# pylint: disable=unused-import
+from h2o4gpu.solvers import elastic_net
+from h2o4gpu.linear_model import ridge as sk
+from ..solvers.utils import _setter
 
-
-class Ridge(GLM):
+class Ridge(object):
     """H2O Ridge Regression Solver
 
-    :param int n_threads: Number of threads to use in the gpu. Default is None.
-
-    :param int n_gpus: Number of gpu's to use in GLM solver. Default is -1.
-
-    :param bool fit_intercept: Include constant term in the model.
-        Default is True.
-
-    :param float lambda_min_ratio: Minimum lambda ratio to maximum lambda, used
-        in lambda search. Default is 1e-7.
-
-    :param int n_lambdas: Number of lambdas to be used in a search.
-        Default is 100.
-
-    :param int n_folds: Number of cross validation folds. Default is 1.
-
-    :param float tol: tolerance.  Default is 1E-2.
-
-    :param float tol_seek_factor : factor of tolerance to seek
-        once below null model accuracy.  Default is 1E-1, so seeks tolerance
-        of 1E-3 once below null model accuracy for tol=1E-2.
-
-    :param bool lambda_stop_early: Stop early when there is no more relative
-        improvement on train or validation. Default is True.
-
-    :param bool glm_stop_early: Stop early when there is no more relative
-        improvement in the primary and dual residuals for ADMM.
-        Default is True.
-
-    :param float glm_stop_early_error_fraction: Relative tolerance for
-        metric-based stopping criterion (stop if relative improvement is not at
-        least this much). Default is 1.0.
-
-    :param int max_iter: Maximum number of iterations. Default is 5000.
-
-    :param int verbose: Print verbose information to the console if set to > 0.
-        Default is 0.
-
-    :param int store_full_path : Extract full regularization path from glm model
-
-    :param str family: Use "logistic" for classification with logistic
-        regression. Defaults to "elasticnet" for regression.
-        Must be "logistic" or "elasticnet".
-
-    :param lambda_max: Maximum Lambda value to use.
-        Default is None, and then internally compute standard maximum
-
-    :param int,float lambdas: list, tuple, array, or numpy 1D array of lambdas,
-        overrides n_lambdas, lambda_max, and lambda_min_ratio. Default is None.
+        Selects between h2o4gpu.solvers.elastic_net.GLM
+        and h2o4gpu.linear_model.coordinate_descent.Ridge_sklearn
+        Documentation:
+        import h2o4gpu.solvers ; help(h2o4gpu.solvers.elastic_net.GLM)
+        help(h2o4gpu.linear_model.ridge.Ridge_sklearn)
     """
 
     def __init__(self,
-                 n_threads=None,
-                 n_gpus=-1,
-                 fit_intercept=True,
-                 lambda_min_ratio=1E-7,
-                 n_lambdas=100,
-                 n_folds=1,
-                 tol=1E-2,
-                 tol_seek_factor=1E-1,
-                 lambda_stop_early=True,
-                 glm_stop_early=True,
-                 glm_stop_early_error_fraction=1.0,
-                 max_iter=5000,
-                 verbose=0,
-                 family="elasticnet",
-                 store_full_path=0,
-                 lambda_max=None,
-                 lambdas=None):
-        super(Ridge, self).__init__(
+                 alpha=1.0, #h2o4gpu
+                 fit_intercept=True, #h2o4gpu
+                 normalize=False,
+                 copy_X=True,
+                 max_iter=5000, #h2o4gpu
+                 tol=1e-2, #h2o4gpu
+                 solver='auto',
+                 random_state=None,
+                 n_gpus=-1,  # h2o4gpu
+                 glm_stop_early=True,  # h2o4gpu
+                 glm_stop_early_error_fraction=1.0, #h2o4gpu
+                 verbose=False): # h2o4gpu
+
+        # Fall back to Sklearn
+        # Can remove if fully implement sklearn functionality
+        self.do_sklearn = False
+
+        params_string = ['normalize', 'copy_X', 'solver', 'random_state']
+        params = [normalize, copy_X, solver, random_state]
+        params_default = [False, True, 'auto', None]
+
+        i = 0
+        self.do_sklearn = False
+        for param in params:
+            if param != params_default[i]:
+                self.do_sklearn = True
+                print("WARNING: The sklearn parameter " + params_string[i]
+                      + " has been changed from default to "
+                      + str(param) + ". Will run Sklearn Ridge Regression.")
+                self.do_sklearn = True
+            i = i + 1
+
+        self.model_sklearn = sk.Ridge_sklearn(
+            alpha=alpha,
+            fit_intercept=fit_intercept,
+            normalize=normalize,
+            copy_X=copy_X,
+            max_iter=max_iter,
+            tol=tol,
+            solver=solver,
+            random_state=random_state)
+
+        n_threads = None
+        n_alphas = 1
+        n_lambdas = 1
+        n_folds = 1
+        lambda_max = None
+        lambda_min_ratio = 1.0
+        lambda_stop_early = False
+        store_full_path = 1
+        alphas = None
+        lambdas = None
+        alpha_min = alpha
+        alpha_max = alpha
+
+        self.model_h2o4gpu = elastic_net.GLM(
             n_threads=n_threads,
             n_gpus=n_gpus,
             fit_intercept=fit_intercept,
             lambda_min_ratio=lambda_min_ratio,
             n_lambdas=n_lambdas,
             n_folds=n_folds,
-            n_alphas=1,
+            n_alphas=n_alphas,
             tol=tol,
-            tol_seek_factor=tol_seek_factor,
             lambda_stop_early=lambda_stop_early,
             glm_stop_early=glm_stop_early,
             glm_stop_early_error_fraction=glm_stop_early_error_fraction,
             max_iter=max_iter,
             verbose=verbose,
-            family=family,
             store_full_path=store_full_path,
             lambda_max=lambda_max,
-            alpha_max=0.0,
-            alpha_min=0.0,
+            alpha_max=alpha_max,
+            alpha_min=alpha_min,
+            alphas=alphas,
             lambdas=lambdas,
             order=None)
+
+        if self.do_sklearn:
+            print("Running sklearn Ridge Regression")
+            self.model = self.model_sklearn
+        else:
+            print("Running h2o4gpu Ridge Regression")
+            self.model = self.model_h2o4gpu
+
+    def fit(self, X, y=None, sample_weight=None):
+        if self.do_sklearn:
+            res = self.model.fit(X, y, sample_weight)
+            self.set_attributes()
+            return res
+        res = self.model.fit(X, y)
+        self.set_attributes()
+        return res
+
+    def get_params(self):
+        return self.model.get_params()
+
+    def predict(self, X):
+        res = self.model.predict(X)
+        self.set_attributes()
+        return res
+
+    def score(self, X, y, sample_weight=None):
+        # TODO add for h2o4gpu
+        print("WARNING: score() is using sklearn")
+        if not self.do_sklearn:
+            self.model_sklearn.fit(X, y) #Need to re-fit
+        res = self.model_sklearn.score(X, y, sample_weight)
+        return res
+
+    def set_params(self, **params):
+        return self.model.set_params(**params)
+
+    def set_attributes(self):
+        s = _setter(oself=self, e1=NameError, e2=AttributeError)
+
+        s('oself.coef_ = oself.model.coef_')
+        s('oself.intercept_ = oself.model.intercept_')
+        s('oself.n_iter_ = oself.model.n_iter_')
