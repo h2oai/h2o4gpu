419f36aa7610642dd836302f63b5d8be6e94bb4f
Jonathan McKinney <pseudotensor@gmail.com>
2017-09-21 16:52:15 -0400

    memory free update -- assume user will expect free during deconstruction/loss of scope or with del and gc.collect() if in python

--- src/gpu/matrix/matrix_dense.cu
+++ src/gpu/matrix/matrix_dense.cu
@@ -642,7 +642,7 @@ MatrixDense<T>::~MatrixDense() {
   checkwDev(_wDev);
   CUDACHECK(cudaSetDevice(_wDev));
 
-  if(0){
+  if(1){
     GpuData<T> *info = reinterpret_cast<GpuData<T>*>(this->_info);
     GpuData<T> *infoy = reinterpret_cast<GpuData<T>*>(this->_infoy);
     GpuData<T> *vinfo = reinterpret_cast<GpuData<T>*>(this->_vinfo);
@@ -658,7 +658,7 @@ MatrixDense<T>::~MatrixDense() {
 
   //  fprintf(stderr,"HERE1\n"); fflush(stderr);
 
-  if(0){ // Note that this frees these pointers as soon as MatrixDense constructor goes out of scope, and might want more fine-grained control over GPU memory if inside (say) high-level python API
+  if(1){ // Note that this frees these pointers as soon as MatrixDense constructor goes out of scope, and might want more fine-grained control over GPU memory if inside (say) high-level python API
     // If 0 is used, then need to ensure user calls a finish() or something to free memory.  If 0, also allows user to call (say) fit() or fitptr() multiple times
     
     if (this->_done_init && _data) {
