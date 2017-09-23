418bfd95cf2d98dcceb368db8615b63fcbeb33ff
navdeep-G <mr.navdeepgill@gmail.com>
2017-09-22 19:19:23 -0700

    Add Dockerfile-runtime

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Note:  This run script is meant to be run inside the docker container.
+#
+
+set -e
+
+source h2o4gpu_env/bin/activate
+
+if [ "x$1" != "x" ]; then
+    d=$1
+    cd $d
+    shift
+    exec "$@"
+fi
+
+logdir=/log/`date "+%Y%m%d-%H%M%S"`
+mkdir -p "$logdir"
+
+export HOME=/jupyter && \
+cd /jupyter && \
+jupyter --paths >> "$logdir"/jupyter.log && \
+exec jupyter notebook --ip='*' --no-browser --allow-root >> "$logdir"/jupyter.log 2>&1
