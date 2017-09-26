cdd621bcb9f45fd232df3fc52f58114bb9889b8b
pseudotensor <pseudotensor@gmail.com>
2017-09-26 16:51:03 -0700

    Update capability list format

--- src/config2.mk
+++ src/config2.mk
@@ -53,6 +53,7 @@ NVCC_GENCODE ?= -gencode=arch=compute_35,code=sm_35 \
                 -gencode=arch=compute_61,code=compute_61 \
                 -gencode=arch=compute_70,code=sm_70 \
                 -gencode=arch=compute_70,code=compute_70
+XGB_CUDA ?= -DGPU_COMPUTE_VER="35;52;60;61;70"
 else
 $(warning Compiling with Cuda8 or lower)
 # >=52 required for kmeans for larger data of size rows/32>2^16
@@ -64,4 +65,6 @@ NVCC_GENCODE ?= -gencode=arch=compute_35,code=sm_35 \
                 -gencode=arch=compute_60,code=compute_60
                 -gencode=arch=compute_61,code=sm_61 \
                 -gencode=arch=compute_61,code=compute_61
+XGB_CUDA ?= -DGPU_COMPUTE_VER="35;52;60;61"
 endif
+
