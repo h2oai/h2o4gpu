6202495d36e6458e4829af280f5861b32e65f589
Mateusz Dymczyk <dymczyk@gmail.com>
2017-11-18 08:38:26 +0900

    [WIP] Improved artefact versioning and clean-up Jenkins build files (#294)
    * Improved artefact versioning:
    * Applied from the original PR due to merge conflicts
    * Pulled out most duplicated code into methods in Jenkinsfile
    * AWS credentials should not be necessary
    * Debug statements in jenkinsfile
    * Remove NonCPS
    * More jenkins debug statements
    * Try running without AWS credentials
    * More jenkins debug statements
    * Test shell call inside method
    * Remove @NonCPS from methods - pipeline steps cannot be called from such methods
    * Add info messages, bring back build script
    * Can't use def fields inside methods
    * Use proper name for docker
    * Dont use utilsLib inside methods
    * Dont delete the snapshot whl - used in later stages
    * Set public acl on artifacts
    * Encode wheel for PIP download
    * Cannot use static method in script, use simple replace instead
    * Fixes stash names
    * Declare versionTag in method
    * Declare versionTag in method
    * Temporarily remove version from stash - figure out how to do it later
    * Merge latest change from master

---
+++ src/interface_py/__about__.py
@@ -0,0 +1,27 @@
+"""
+:copyright: 2017 H2O.ai, Inc.
+:license:   Apache License Version 2.0 (see LICENSE for details)
+"""
+__all__ = [ "__version__", "__build_info__" ]
+
+# Build defaults
+build_info = {
+    'suffix'    : '+local',
+    'build'     : 'dev',
+    'commit'    : '',
+    'describe'  : '',
+    'build_os'  : '',
+    'build_machine' : '',
+    'build_date' : '',
+    'build_user' : '',
+    'base_version' : '0.0.0'
+}
+
+import pkg_resources
+path = pkg_resources.resource_filename("h2o4gpu", "BUILD_INFO.txt")
+if pkg_resources.os.path.exists(path):
+    with open(path) as f: exec(f.read(), build_info)
+
+# Exported properties to make them available in __init__.py
+__version__ = "{}{}".format(build_info['base_version'], build_info['suffix'])
+__build_info__ = build_info
