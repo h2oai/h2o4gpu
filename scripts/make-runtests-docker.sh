27415d6317a693cd3fc3a0ff0ddbe9b157dbf15c
pseudotensor <pseudotensor@gmail.com>
2017-11-20 14:44:39 -0800

    more

--- scripts/make-runtests-docker.sh
+++ scripts/make-runtests-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 nvidia-docker build  -t opsh2oai/h2o4gpu-${extratag}-build -f Dockerfile-build --rm=false --build-arg cuda=${dockerimage} .
 nvidia-docker run  --init --rm --name ${CONTAINER_NAME} -d -t -u `id -u`:`id -g` -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -w `pwd` -v `pwd`:`pwd`:rw --entrypoint=bash opsh2oai/h2o4gpu-${extratag}-build
-nvidia-docker exec ${CONTAINER_NAME} rm -rf data
-nvidia-docker exec ${CONTAINER_NAME} ln -s /data ./data
-nvidia-docker exec ${CONTAINER_NAME} rm -rf open_data
-nvidia-docker exec ${CONTAINER_NAME} ln -s /open_data ./open_data
+nvidia-docker exec ${CONTAINER_NAME} rm -rf data/*
+nvidia-docker exec ${CONTAINER_NAME} ln -sf /data ./data
+nvidia-docker exec ${CONTAINER_NAME} rm -rf open_data/*
+nvidia-docker exec ${CONTAINER_NAME} ln -sf /open_data ./open_data
 nvidia-docker exec ${CONTAINER_NAME} rm -rf py3nvml
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval \"\$(/root/.pyenv/bin/pyenv init -)\"  ; /root/.pyenv/bin/pyenv global 3.6.1; pip install `find src/interface_py/${dist} -name "*h2o4gpu-${versionTag}*.whl"`; make ${target}'
 nvidia-docker exec ${CONTAINER_NAME} touch src/interface_py/h2o4gpu/__init__.py
