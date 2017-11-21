c157714ad111d0c50e0709cdbd44690dea3fee33
pseudotensor <pseudotensor@gmail.com>
2017-11-20 19:37:14 -0800

    updates15

--- scripts/make-docker-runtests.sh
+++ scripts/make-docker-runtests.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+
 echo "Docker devel test and pylint - BEGIN"
 nvidia-docker build  -t opsh2oai/h2o4gpu-${versionTag}${extratag}-build -f Dockerfile-build --rm=false --build-arg cuda=${dockerimage} .
 #-u `id -u`:`id -g`  -w `pwd` -v `pwd`:`pwd`:rw
 nvidia-docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-${versionTag}${extratag}-build
+
 echo "Docker devel test and pylint - Copying files"
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo ; cd ./repo ; ln -sf /data . ; ln -sf /open_data .'
+
 echo "Docker devel test and pylint - setup pyenv, pip install from ${dist} wheel with versionTag=${versionTag}, make ${target}"
-fullwheel=`find /dot/src/interface_py/${dist} -name "*h2o4gpu-*.whl"`
+
+
 echo "fullwheel=$fullwheel"
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval "$(/root/.pyenv/bin/pyenv init -)" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; pip install '$fullwheel'; pip freeze ; make '${target}
+nvidia-docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval "$(/root/.pyenv/bin/pyenv init -)" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; pip install `find /dot/src/interface_py/'${dist}' -name "*h2o4gpu-*.whl"`; pip freeze ; make '${target}
 nvidia-docker exec ${CONTAINER_NAME} touch ./repo/src/interface_py/h2o4gpu/__init__.py
+
 echo "Docker devel test and pylint - pylint"
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'eval "$(/root/.pyenv/bin/pyenv init -)"  ;  /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; make pylint'
+
 echo "Docker devel test and pylint - copy build reports"
 rm -rf build/test-reports ; mkdir -p build/test-reports/
 nvidia-docker cp -a ${CONTAINER_NAME}:repo/build/test-reports build/
+
 echo "Docker devel test and pylint - stop"
 nvidia-docker stop ${CONTAINER_NAME}
--- scripts/make-docker-runtime.sh
+++ scripts/make-docker-runtime.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 echo "Docker runtime - BEGIN"
+
 echo "Docker runtime - Will get wheel from S3 (i.e. not local wheel)"
 nvidia-docker build -t opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime:latest -f Dockerfile-runtime --rm=false --build-arg cuda=${dockerimage} --build-arg wheel=${encodedFullVersionTag}${extratag}/h2o4gpu-${encodedFullVersionTag}-py36-none-any.whl --build-arg buckettype=${buckettype} .
 # -u `id -u`:`id -g` -d -t -w `pwd` -v `pwd`:`pwd`:rw
+
 echo "Runtime Docker - Run"
 nvidia-docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime:latest
 
 echo "Docker runtime - pip freeze"
 nvidia-docker exec ${CONTAINER_NAME} bash -c '. /h2o4gpu_env/bin/activate ; pip freeze'
+
 echo "Docker runtime - Getting Data"
 nvidia-docker exec ${CONTAINER_NAME} bash -c '. /h2o4gpu_env/bin/activate ; mkdir -p scripts ; rm -rf scripts/fcov_get.py ; echo "from sklearn.datasets import fetch_covtype" > ./scripts/fcov_get.py ; echo "cov = fetch_covtype()" >> ./scripts/fcov_get.py'
 nvidia-docker exec ${CONTAINER_NAME} bash -c '. /h2o4gpu_env/bin/activate ; python ./scripts/fcov_get.py'
@@ -16,8 +19,11 @@ nvidia-docker exec ${CONTAINER_NAME} bash -c 'cd /jupyter/demos ; wget https://s
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'cd /jupyter/demos ; cp /data/ipums_1k.csv .'
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'cd /jupyter/demos ; cp /data/ipums.feather .'
 nvidia-docker commit ${CONTAINER_NAME} opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime:latest
+
 echo "Docker runtime - stopping docker"
 nvidia-docker stop ${CONTAINER_NAME}
+
 echo "Docker runtime - saving docker"
 nvidia-docker save opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime | gzip > h2o4gpu-${fullVersionTag}${extratag}-runtime.tar.gz
-echo "Docker runtime Done"
+
+echo "Docker runtime - END"
