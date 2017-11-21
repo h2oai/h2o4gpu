0687221f1fd63c7db984ab255580a8204442cf37
pseudotensor <pseudotensor@gmail.com>
2017-11-20 19:03:42 -0800

    updates2

--- scripts/make-docker-runtests.sh
+++ scripts/make-docker-runtests.sh
@@ -7,6 +7,6 @@ echo "Docker devel test and pylint - Copying files"
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo ; cd ./repo ; ln -sf /data . ; ln -sf /open_data .'
 echo "setup pyenv, make ${target}, and make pylint"
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval "$(/root/.pyenv/bin/pyenv init -)" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; pip install `find src/interface_py/${dist} -name "*h2o4gpu-${versionTag}*.whl"`; make ${target}'
-nvidia-docker exec ${CONTAINER_NAME} touch src/interface_py/h2o4gpu/__init__.py
+nvidia-docker exec ${CONTAINER_NAME} touch ./repo/src/interface_py/h2o4gpu/__init__.py
 nvidia-docker exec ${CONTAINER_NAME} bash -c 'eval "$(/root/.pyenv/bin/pyenv init -)"  ;  /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; make pylint'
 nvidia-docker stop ${CONTAINER_NAME}
