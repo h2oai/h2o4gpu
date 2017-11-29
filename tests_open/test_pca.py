affc60b7371e71ee22a486e9680fd5c1f0044db1
navdeep-G <mr.navdeepgill@gmail.com>
2017-11-29 13:58:12 -0800

    Add proper calcuations of explained variance and components as they differ from SVD

--- tests_open/test_pca.py
+++ tests_open/test_pca.py
@@ -11,8 +11,9 @@ logging.basicConfig(level=logging.DEBUG)
 def func(m=5000000, n=10, k=9):
     np.random.seed(1234)
 
-    X = np.array([[2,4,3], [1,5,7]])
+    X = np.array([[2,4,3], [1,5,7], [3,6,8]])
     X = np.transpose(X)
+    print(X.shape)
     #X = np.random.rand(m, n)
 
     print("\n")
@@ -20,10 +21,28 @@ def func(m=5000000, n=10, k=9):
     print(np.mean(X, axis=0))
 
     h2o4gpu_pca = PCAH2O(n_components=k)
-    scikit_pca = PCA(n_components=k, svd_solver="full")
+    scikit_pca = PCA(n_components=k, svd_solver="arpack")
     scikit_pca.fit(X)
     h2o4gpu_pca.fit(X)
-    print(h2o4gpu_pca.explained_variance)
+
+    print("Explained variance")
+    print(h2o4gpu_pca.explained_variance_)
     print(scikit_pca.explained_variance_)
 
-def test_pca_error_k2(): func(m=5000000, n=10, k=1)
+    print("Explained variance ratio")
+    print(h2o4gpu_pca.explained_variance_ratio_)
+    print(scikit_pca.explained_variance_ratio_)
+
+    print("Singular values")
+    print(h2o4gpu_pca.singular_values_)
+    print(scikit_pca.singular_values_)
+
+    print("Components")
+    print(h2o4gpu_pca.components_)
+    print(scikit_pca.components_)
+
+    print("Num components")
+    print(h2o4gpu_pca.n_components)
+    print(scikit_pca.n_components)
+
+def test_pca_error_k2(): func(m=5000000, n=10, k=2)
