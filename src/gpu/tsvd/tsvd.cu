e55c6e827efe284e44fc79e3bf73ab42f3e446de
navdeep-G <mr.navdeepgill@gmail.com>
2017-11-29 16:55:39 -0800

    Pass in correct matrix to avoid OOM

--- src/gpu/tsvd/tsvd.cu
+++ src/gpu/tsvd/tsvd.cu
@@ -242,7 +242,7 @@ void truncated_svd(const double* _X, double* _Q, double* _w, double* _U, double*
 {
     Matrix<float>X(_param.X_m, _param.X_n);
 	X.copy(_X);
-    truncated_svd(_X, _Q, _w, _U, _explained_variance, _explained_variance_ratio, _param);
+    truncated_svd_matrix(X, _Q, _w, _U, _explained_variance, _explained_variance_ratio, _param);
 }
 
 void truncated_svd_matrix(Matrix<float> X, double* _Q, double* _w, double* _U, double* _explained_variance, double* _explained_variance_ratio, params _param)
