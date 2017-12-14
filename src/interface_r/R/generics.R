0f6e84d56b0493a8fbf29e27ad962f6d2fdbe623
terrytangyuan <terrytangyuan@gmail.com>
2017-12-14 09:09:23 -0800

    Fixed generic methods and added attach_attrs_to_model

--- src/interface_r/R/generics.R
+++ src/interface_r/R/generics.R
@@ -1,12 +1,4 @@
 #' @export
-h2o4gpu_model <- function(model, subclass = NULL) {
-  structure(
-    list(model = model),
-    class = c(subclass, "h2o4gpu_model")
-  )
-}
-
-#' @export
 fit <- function(object, ...) {
   UseMethod("fit")
 }
--- src/interface_r/tests/testthat/test_model.R
+++ src/interface_r/tests/testthat/test_model.R
@@ -1,14 +1,17 @@
 context("Test h2o4gpu_model methods")
 
-test_that("kmeans model can be constructed and trained", {
+test_that("kmeans model works correctly", {
   np <- reticulate::import("numpy")
   
   x <- np$array(list(list(1, 1), list(1, 4), list(1, 0)))
   model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(x)
-  predictions <- model %>% predict(x)
-  transformed <- model %>% transform(x)
-  model$model$cluster_centers_
+  expect_equal(model$model$cluster_centers_, structure(c(1, 1, 0.5, 4), .Dim = c(2L, 2L)))
   
-  # Test support to fit in data.frame
+  # Test other generic methods
+  expect_equal(model %>% predict(x), structure(c(0L, 1L, 0L), .Dim = 3L))
+  expect_equal(model %>% transform(x), structure(c(0.5, 3.5, 0.5, 3, 0, 4), .Dim = c(3L, 2L)))
+  expect_equal(model %>% score(x), -0.5)
+  
+  # Test support for data.frame
   model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(as.data.frame(x))
 })
