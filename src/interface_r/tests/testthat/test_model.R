78c3619614446bef0aa0a2848941047073366729
terrytangyuan <terrytangyuan@gmail.com>
2017-12-18 08:55:13 -0800

    Added tests for different inputs for all generic methods

--- src/interface_r/tests/testthat/test_model.R
+++ src/interface_r/tests/testthat/test_model.R
@@ -2,23 +2,22 @@ context("Test h2o4gpu_model methods")
 
 np <- reticulate::import("numpy")
 
-test_that("Generic methods for model work correctly", {
-  x <- np$array(list(list(1, 1), list(1, 4), list(1, 0)))
-
-  model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(x)
+test_generic_methods <- function(input) {
+  model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(input)
   expect_equal(model$cluster_centers_, structure(c(1, 1, 0.5, 4), .Dim = c(2L, 2L)))
-  expect_equal(model %>% predict(x), structure(c(0L, 1L, 0L), .Dim = 3L))
-  expect_equal(model %>% transform(x), structure(c(0.5, 3.5, 0.5, 3, 0, 4), .Dim = c(3L, 2L)))
-  expect_equal(model %>% score(x), -0.5)
-})
+  expect_equal(model %>% predict(input), structure(c(0L, 1L, 0L), .Dim = 3L))
+  expect_equal(model %>% transform(input), structure(c(0.5, 3.5, 0.5, 3, 0, 4), .Dim = c(3L, 2L)))
+  expect_equal(model %>% score(input), -0.5)
+}
 
-test_that("Models accept different types of inputs", {
+test_that("Generic methods for model work correctly for different types of inputs", {
   raw_input <- list(list(1, 1), list(1, 4), list(1, 0))
-  x <- np$array(list(list(1, 1), list(1, 4), list(1, 0)))
+  x <- np$array(raw_input)
   
   # Test support for data.frame
-  model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(as.data.frame(x))
-
+  test_generic_methods(as.data.frame(x))
   # Test support for list
-  model <- h2o4gpu.kmeans(n_clusters = 2L, random_state = 1234L) %>% fit(raw_input)
+  test_generic_methods(raw_input)
+  # Test support for matrix/np.array
+  test_generic_methods(x)
 })
