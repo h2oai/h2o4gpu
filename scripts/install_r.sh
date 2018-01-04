6003dc6d093c54747051d7f622b4dceaef7ef912
Yuan (Terry) Tang <terrytangyuan@users.noreply.github.com>
2018-01-04 15:05:54 -0500

    Add R tests in CI build process (#370)
    * Added R tests in build process
    
    * Install roxygen2 from github
    
    * Fix
    
    * Do not use build cache
    
    * Try running install + test together
    
    * Install libxml 2
    
    * Fix R libPath issue
    
    * Use the correct virtualenv
    
    * Switch to use pyenv
    
    * Enable shared lib when install pyenv environment
    
    * Added test utilities

---
+++ scripts/install_r.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+R_VERSION=$1
+R_VERSION_HOME="/usr/local/R/${R_VERSION}"
+
+# Create temporary directory
+mkdir -p $HOME/R_tmp
+cd $HOME/R_tmp
+
+# Download and extract R source
+echo Downloading R source
+wget https://cran.r-project.org/src/base/R-3/R-${R_VERSION}.tar.gz
+tar xvzf R-${R_VERSION}.tar.gz
+rm R-${R_VERSION}.tar.gz
+
+# Configure and make
+cd R-${R_VERSION}
+./configure --prefix=${R_VERSION_HOME} --with-x=no --enable-utf
+make
+make install
+chmod a+w -R ${R_VERSION_HOME}/lib/R/library
+
+# Cleanup
+cd ../..
+rm -rf $HOME/R_tmp
+
+# Create symbolic link
+ln -s ${R_VERSION_HOME}/bin/R /usr/bin/R-${R_VERSION}
+ln -s ${R_VERSION_HOME}/bin/Rscript /usr/bin/Rscript-${R_VERSION}
+echo The R-${R_VERSION} executable is now available in /usr/bin/R-${R_VERSION}
+
+# Create dirs for current R and make them writable
+mkdir -p /usr/local/R/current/bin/
+chmod a+w /usr/local/R/current/bin/
+
+echo "Generate activate script"
+# Create activation script
+echo """#! /bin/bash
+ln -s -f ${R_VERSION_HOME}/bin/R /usr/local/R/current/bin/R
+ln -s -f ${R_VERSION_HOME}/bin/Rscript /usr/local/R/current/bin/Rscript""" > /usr/bin/activate_R_${R_VERSION}
+chmod a+x /usr/bin/activate_R_${R_VERSION}
+
+# Activate this R version
+echo "Activating R ${R_VERSION}"
+activate_R_${R_VERSION}
