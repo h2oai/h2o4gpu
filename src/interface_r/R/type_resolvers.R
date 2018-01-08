636aa4395daf81e1492f3235d97cf8dc5845767d
pseudotensor <pseudotensor@gmail.com>
2018-01-08 13:23:51 -0800

    Revert "Support supervised models and added XGBoost random_forest_classifier (#374)"
    This reverts commit bee0c8fbe53ab99df523345ac1dad309e2d29943.

--- src/interface_r/R/type_resolvers.R
+++ src/interface_r/R/type_resolvers.R
@@ -6,7 +6,7 @@ as_nullable_integer <- function(x) {
 }
 
 resolve_model_input <- function(x) {
-  if (is.matrix(x) || is.numeric(x)) {
+  if (is.matrix(x)) {
     x
   } else if (is.data.frame(x)) {
     as.matrix(x)
@@ -14,13 +14,5 @@ resolve_model_input <- function(x) {
     np$array(x)
   } else {
     stop(paste0("Input x of type \"", class(x), "\" is not currently supported."))
-  }
-}
-
-resolve_model_y <- function(y) {
-  if (is.null(y)) {
-    NULL
-  } else {
-    resolve_model_input(y)
   }
 }
