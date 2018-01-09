3236c2b207d124ca63d88aa1fbe9a5f8361cc5ca
terrytangyuan <terrytangyuan@gmail.com>
2018-01-09 14:19:24 -0800

    Modified tests

--- src/interface_r/tests/testthat/test_xgboost.R
+++ src/interface_r/tests/testthat/test_xgboost.R
@@ -3,11 +3,16 @@ context("Test xgboost models")
 source("helper-utils.R")
 
 test_random_forest_classifier <- function(x, y) {
-  model <- h2o4gpu.random_forest_classifier() %>% fit(x, y)
-  predictions <- model %>% predict(x)
-  expect_equal(max(predictions), 1)
-  expect_equal(min(predictions), 0)
-  expect_true(model %>% score(x, y) > 0.90)
+  # Suppress intentional warnings
+  suppressWarnings(
+    {
+      model <- h2o4gpu.random_forest_classifier() %>% fit(x, y)
+      predictions <- model %>% predict(x)
+      expect_equal(max(predictions), 1)
+      expect_equal(min(predictions), 0)
+      expect_true(model %>% score(x, y) > 0.90)
+    }
+  )
 }
 
 test_succeeds("Random Forest classifier works correctly with numeric labels", {
