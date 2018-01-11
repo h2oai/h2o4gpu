fea2b90da40f1a82c1f76a7646bfb7b35ffc40f0
Yuan (Terry) Tang <terrytangyuan@users.noreply.github.com>
2018-01-11 12:32:43 -0500

    [R] Support character/factor columns for labels (#382)
    * Support character/factor labels
    
    * Modified tests

--- src/interface_r/tests/testthat/test_xgboost.R
+++ src/interface_r/tests/testthat/test_xgboost.R
@@ -2,14 +2,27 @@ context("Test xgboost models")
 
 source("helper-utils.R")
 
-test_succeeds("Random Forest classifier works correctly", {
+test_random_forest_classifier <- function(x, y) {
+  # Suppress intentional warnings
+  suppressWarnings(
+    {
+      model <- h2o4gpu.random_forest_classifier() %>% fit(x, y)
+      predictions <- model %>% predict(x)
+      expect_equal(max(predictions), 1)
+      expect_equal(min(predictions), 0)
+      expect_true(model %>% score(x, y) > 0.90)
+    }
+  )
+}
 
+test_succeeds("Random Forest classifier works correctly with numeric labels", {
   x <- iris[1:4]
   y <- as.integer(iris$Species) - 1
+  test_random_forest_classifier(x, y)
+})
 
-  model <- h2o4gpu.random_forest_classifier() %>% fit(x, y)
-  predictions <- model %>% predict(x)
-  expect_equal(max(predictions), 1)
-  expect_equal(min(predictions), 0)
-  expect_true(model %>% score(x, y) > 0.90)
+test_succeeds("Random Forest classifier works correctly with factor labels", {
+  x <- iris[1:4]
+  y <- iris$Species
+  test_random_forest_classifier(x, y)
 })
