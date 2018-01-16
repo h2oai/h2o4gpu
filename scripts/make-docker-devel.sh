df53ef977aa634ca170b441c7c8a4e76299876a4
Monika vos Mueller <monika.vos.mueller@gmail.com>
2018-01-16 14:26:46 -0800

    CPU build for H2O4GPU

--- scripts/make-docker-devel.sh
+++ scripts/make-docker-devel.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
 set -e
 
+# split layer and version
+IFS=':' read -ra LAYER_VERSION <<< "${dockerimage}"
+layer=${LAYER_VERSION[0]}
+version=${LAYER_VERSION[1]}
+
+if [ "$layer" == "ubuntu" ]
+then
+	docker=docker
+else
+	docker=nvidia-docker
+fi
+
+
 #--build-arg http_proxy=http://172.16.2.142:3128/
 echo "Docker devel - BEGIN"
-nvidia-docker build  -t opsh2oai/h2o4gpu-buildversion${extratag}-build -f Dockerfile-build --rm=false --build-arg cuda=${dockerimage} .
+$docker build  -t opsh2oai/h2o4gpu-buildversion${extratag}-build -f Dockerfile-build --rm=false --build-arg layer=$layer --build-arg version=$version .
 #-u `id -u`:`id -g`  -w `pwd` -v `pwd`:`pwd`:rw
-nvidia-docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
+$docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
+
+exit
 
 echo "Docker devel - Copying files"
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
+$docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
 
 echo "setup pyenv, shallow clone, and make fullinstalljenkins with ${H2O4GPU_BUILD} and ${H2O4GPU_SUFFIX}"
-nvidia-docker exec ${CONTAINER_NAME} bash -c "eval \"\$(/root/.pyenv/bin/pyenv init -)\" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; ./scripts/gitshallow_submodules.sh ; make ${makeopts} fullinstalljenkins${extratag} H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
+$docker exec ${CONTAINER_NAME} bash -c "eval \"\$(/root/.pyenv/bin/pyenv init -)\" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; ./scripts/gitshallow_submodules.sh ; make ${makeopts} fullinstalljenkins${extratag} H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
 
 echo "Docker devel - Clean local wheels and Copying wheel from docker"
 rm -rf src/interface_py/${dist}/*.whl
-nvidia-docker cp -a ${CONTAINER_NAME}:repo/src/interface_py/${dist} src/interface_py/
+$docker cp -a ${CONTAINER_NAME}:repo/src/interface_py/${dist} src/interface_py/
 
 echo "Docker devel - Copying VERSION.txt"
-mkdir -p build ; nvidia-docker cp ${CONTAINER_NAME}:repo/build/VERSION.txt build/
+mkdir -p build ; $docker cp ${CONTAINER_NAME}:repo/build/VERSION.txt build/
 
 echo "Docker devel - Stopping docker"
-nvidia-docker stop ${CONTAINER_NAME}
+$docker stop ${CONTAINER_NAME}
 
 echo "Docker devel - END"
--- scripts/make-docker-runtests.sh
+++ scripts/make-docker-runtests.sh
@@ -2,33 +2,46 @@
 # Requires one has already done(e.g.): make docker-build-nccl-cuda9 to get wheel built or wheel was unstashed on jenkins
 set -e
 
+# split layer and version
+IFS=':' read -ra LAYER_VERSION <<< "${dockerimage}"
+layer=${LAYER_VERSION[0]}
+version=${LAYER_VERSION[1]}
+
+if [ "$layer" == "ubuntu" ]
+then
+	docker=docker
+else
+	docker=nvidia-docker
+fi
+
+
 echo "Docker devel test and pylint - BEGIN"
 # --build-arg http_proxy=http://172.16.2.142:3128/
-nvidia-docker build  -t opsh2oai/h2o4gpu-buildversion${extratag}-build -f Dockerfile-build --rm=false --build-arg cuda=${dockerimage} .
+$docker build  -t opsh2oai/h2o4gpu-buildversion${extratag}-build -f Dockerfile-build --rm=false --build-arg layer=$layer --build-arg version=$version .
 #-u `id -u`:`id -g`  -w `pwd` -v `pwd`:`pwd`:rw
-nvidia-docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
+$docker run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v /home/0xdiag/h2o4gpu/data:/data -v /home/0xdiag/h2o4gpu/open_data:/open_data -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
 
 echo "Docker devel test and pylint - Copying files"
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'cd ./repo ; ln -sf /data . || true ; ln -sf /open_data . || true'
+$docker exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
+$docker exec ${CONTAINER_NAME} bash -c 'cd ./repo ; ln -sf /data . || true ; ln -sf /open_data . || true'
 
 echo "Docker devel test and pylint - setup pyenv, pip install wheel from ${dist}, make ${target}"
 
 # Don't use version in wheel name when find so local call to this script works without specific jenkins versions
 # Just ensure clean ${dist}/*.whl before unstash in jenkins
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval "$(/root/.pyenv/bin/pyenv init -)" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; pip install `find /dot/src/interface_py/'${dist}' -name "*h2o4gpu-*.whl"`; pip freeze ; make '${target}
+$docker exec ${CONTAINER_NAME} bash -c 'export HOME=`pwd`; eval "$(/root/.pyenv/bin/pyenv init -)" ; /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; pip install `find /dot/src/interface_py/'${dist}' -name "*h2o4gpu-*.whl"`; pip freeze ; make '${target}
 
 echo "Docker devel test and pylint - copy build reports"
 rm -rf build/test-reports ; mkdir -p build/test-reports/
-nvidia-docker cp -a ${CONTAINER_NAME}:repo/build/test-reports build/
+$docker cp -a ${CONTAINER_NAME}:repo/build/test-reports build/
 
 echo "Docker devel test and pylint - copy logs for arch"
 rm -rf tmp ; mkdir -p tmp
-nvidia-docker cp -a ${CONTAINER_NAME}:repo/tmp ./
+$docker cp -a ${CONTAINER_NAME}:repo/tmp ./
 
 echo "Docker devel test and pylint - pylint"
-nvidia-docker exec ${CONTAINER_NAME} touch ./repo/src/interface_py/h2o4gpu/__init__.py
-nvidia-docker exec ${CONTAINER_NAME} bash -c 'eval "$(/root/.pyenv/bin/pyenv init -)"  ;  /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; make pylint'
+$docker exec ${CONTAINER_NAME} touch ./repo/src/interface_py/h2o4gpu/__init__.py
+$docker exec ${CONTAINER_NAME} bash -c 'eval "$(/root/.pyenv/bin/pyenv init -)"  ;  /root/.pyenv/bin/pyenv global 3.6.1; cd repo ; make pylint'
 
 echo "Docker devel test and pylint - stop"
-nvidia-docker stop ${CONTAINER_NAME}
+$docker stop ${CONTAINER_NAME}
