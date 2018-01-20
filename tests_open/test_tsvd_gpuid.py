111c7a048a03aa059254eb78eefd4b723c1e14dd
navdeep-G <mr.navdeepgill@gmail.com>
2018-01-19 16:13:59 -0800

    Add test for gpu id if num gpus > 0

---
+++ tests_open/test_tsvd_gpuid.py
@@ -0,0 +1,45 @@
+import numpy as np
+import time
+import sys
+import logging
+from h2o4gpu.solvers import TruncatedSVDH2O
+from h2o4gpu.util import gpu
+
+print(sys.path)
+
+logging.basicConfig(level=logging.DEBUG)
+
+def func(m=5000, n=10, k=9):
+    np.random.seed(1234)
+
+    X = np.random.rand(m, n)
+
+    gpu_id = 0
+
+    total_gpu, total_mem = gpu.get_gpu_info()
+
+    if(total_gpu > 0):
+        gpu_id = 1 #Use second gpu
+
+    print("\n")
+    print("SVD on gpu id -> " + str(gpu_id))
+    print("SVD on " + str(X.shape[0]) + " by " + str(X.shape[1]) + " matrix")
+    print("Original X Matrix")
+    print(X)
+    print("\n")
+    print("h2o4gpu tsvd run")
+    start_time = time.time()
+    h2o4gpu_tsvd = TruncatedSVDH2O(n_components=k, gpu_id=gpu_id)
+    h2o4gpu_tsvd.fit(X)
+    end_time = time.time() - start_time
+    print("Total time for h2o4gpu tsvd is " + str(end_time))
+    print("h2o4gpu tsvd Singular Values")
+    print(h2o4gpu_tsvd.singular_values_)
+    print("h2o4gpu tsvd Components (V^T)")
+    print(h2o4gpu_tsvd.components_)
+    print("h2o4gpu tsvd Explained Variance")
+    print(h2o4gpu_tsvd.explained_variance_)
+    print("h2o4gpu tsvd Explained Variance Ratio")
+    print(h2o4gpu_tsvd.explained_variance_ratio_)
+
+def test_tsvd_gpu_id(): func(k=5)
