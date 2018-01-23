80cafb0af92fd0e1cbbf085a1398a0bdae57e5b7
Yuan (Terry) Tang <terrytangyuan@users.noreply.github.com>
2018-01-22 19:04:58 -0500

    More directly write wrappers to a file instead of relying on sink() (#398)

--- src/interface_r/scripts/gen_wrapper_utils.R
+++ src/interface_r/scripts/gen_wrapper_utils.R
@@ -6,53 +6,75 @@ gen_wrapper <- function(
   nullable_int_params = NULL,
   class_tags = NULL) {
 
-  docs <- reticulate::py_function_docs(python_function)
-  con <- textConnection("wrapper", "w")
-
-  write("#' @export", file = con)
-  if (is.null(r_function)) {
-    r_function <- docs$name
+  write_line <- function(text) {
+    cat(text, sep = "\n")
   }
 
-  # Generate function signature
-  signature <- sub(paste0(docs$name, "\\("),
-                   paste(r_function, "<- function(\n\t"), docs$signature)
-  signature <- gsub(', ', ',\n\t', signature)
-  write(paste(signature, "{\n"), file = con)
-  
-  # Execution of Python API
-  write(paste0("  model <- ", python_function, "("), file = con)
-  
-  params <- names(docs$parameters)
-  # Extract the params with integer types
-  int_params <- gsub(" = [-]?[0-9]+L", "", stringr::str_extract_all(signature, "[A-z]+ = [-]?[0-9]+L")[[1]])
-  if (!is.null(additional_int_params)) int_params <- c(int_params, additional_int_params)
-
-  # Generate parameters that get passed to the Python API call
-  if (length(params) > 0) {
-    for (i in 1:length(params)) {
-      param <- params[i]
-      # Custom handling of integer params
-      if (param %in% int_params) {
-        param <- paste0("as.integer(", param, ")")
+  capture.output(
+    {
+      docs <- reticulate::py_function_docs(python_function)
+      
+      write_line("#' @export")
+      if (is.null(r_function)) {
+        r_function <- docs$name
       }
-      # Custom handling of nullable integer params
-      if (!is.null(nullable_int_params) && param %in% nullable_int_params) {
-        param <- paste0("as_nullable_integer(", param, ")")
+      
+      # Generate function signature
+      signature <- sub(paste0(docs$name, "\\("),
+                       paste(r_function, "<- function(\n\t"), docs$signature)
+      signature <- gsub(', ', ',\n\t', signature)
+      write_line(paste(signature, "{\n"))
+      
+      # Execution of Python API
+      write_line(paste0("  model <- ", python_function, "("))
+      
+      params <- names(docs$parameters)
+      # Extract the params with integer types
+      int_params <- gsub(" = [-]?[0-9]+L", "", stringr::str_extract_all(signature, "[A-z]+ = [-]?[0-9]+L")[[1]])
+      if (!is.null(additional_int_params)) int_params <- c(int_params, additional_int_params)
+      
+      # Generate parameters that get passed to the Python API call
+      if (length(params) > 0) {
+        for (i in 1:length(params)) {
+          param <- params[i]
+          # Custom handling of integer params
+          if (param %in% int_params) {
+            param <- paste0("as.integer(", param, ")")
+          }
+          # Custom handling of nullable integer params
+          if (!is.null(nullable_int_params) && param %in% nullable_int_params) {
+            param <- paste0("as_nullable_integer(", param, ")")
+          }
+          suffix <- ifelse(i < length(params), ",", "\n  )")
+          write_line(paste0("    ", params[[i]], " = ", param, suffix))
+        }
+      } else {
+        write_line(")")
       }
-      suffix <- ifelse(i < length(params), ",", "\n  )")
-      write(paste0("    ", params[[i]], " = ", param, 
-                   suffix), file = con)
+      
+      # Attach additional class information
+      write_line(paste0('  h2o4gpu_model(model, ', class_tags, ')'))
+      
+      write_line("}\n")
     }
-  } else {
-    write(")", file = con)
-  }
+  )
+}
 
-  # Attach additional class information
-  write(paste0('  h2o4gpu_model(model, ', class_tags, ')'), file = con)
-
-  write("}\n", file = con)
-  close(con)
-  class(wrapper) <- c("py_wrapper", "character")
-  wrapper
+# Append the wrapper 
+write_wrapper <- function(python_function,
+                          file_name,
+                          r_function = NULL,
+                          additional_int_params = NULL,
+                          nullable_int_params = NULL,
+                          class_tags = NULL) {
+  write(
+    gen_wrapper(
+      python_function,
+      r_function,
+      additional_int_params,
+      nullable_int_params,
+      class_tags
+      ),
+    file = file_name,
+    append = TRUE)
 }
