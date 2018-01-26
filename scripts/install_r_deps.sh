38e7e0b138bfddb185ad53d73e72fbae85045c95
terrytangyuan <terrytangyuan@gmail.com>
2018-01-26 15:36:14 -0800

    Use older version of testthat to avoid false positives

--- scripts/install_r_deps.sh
+++ scripts/install_r_deps.sh
@@ -15,5 +15,5 @@ else
 fi
 
 echo "Installing necessary R packages..."
-R -e 'options(repos = c(CRAN = "http://cran.rstudio.com")); pkgs <- c("devtools", "testthat", "magrittr", "roxygen2"); pkgs_to_install <- pkgs[!pkgs %in% row.names(installed.packages())]; if (length(pkgs_to_install) != 0) install.packages(pkgs_to_install); if (!require("reticulate")) devtools::install_github("rstudio/reticulate")'
+R -e 'options(repos = c(CRAN = "http://cran.rstudio.com")); pkgs <- c("devtools", "magrittr", "roxygen2"); pkgs_to_install <- pkgs[!pkgs %in% row.names(installed.packages())]; if (length(pkgs_to_install) != 0) install.packages(pkgs_to_install); if (!require("reticulate")) devtools::install_github("rstudio/reticulate"); if (!require("testthat")) devtools::install_github("r-lib/testthat@v1.0.2")'
 R -e 'devtools::install_local("src/interface_r/")'
