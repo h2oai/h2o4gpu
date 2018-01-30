202d077058baea0c6d45c7a30d94230a2fdf5e8c
Monika vos Mueller <monika.vos.mueller@gmail.com>
2018-01-29 17:32:28 -0800

    Daal installation script added

--- scripts/daal/install_daal_in_docker.sh
+++ scripts/daal/install_daal_in_docker.sh
@@ -6,12 +6,10 @@
 #===============================================================================
 _indel_dall_tar="https://s3.amazonaws.com/intel-daal/daal-linux_x86_64.tar.gz"
 _daal_root="$HOME/daal"
-_pwd=$PWD
 
 apt-get install axel -y
 mkdir -p $_daal_root
 cd /tmp/ && axel -a -n 20 $_indel_dall_tar && tar -xzvf daal-linux_x86_64.tar.gz -C $HOME
 echo "** Successfully downloaded DAAL. **"
 cd $_daal_root && pip install pydaal-2018*.whl && export LD_LIBRARY_PATH=$_daal_root/lib:$LD_LIBRARY_PATH && cd $PWD
-cd $_pwd
 echo "** The DAAL environment is all set up. **"
