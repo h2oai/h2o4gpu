ab24c462f2ee843ff915fa58584c96e31bc32368
Monika vos Mueller <monika.vos.mueller@gmail.com>
2018-03-10 08:22:08 -0800

    conditionally import daal library

--- tests_open/daal/test_daal_normalization.py
+++ tests_open/daal/test_daal_normalization.py
@@ -1,45 +1,51 @@
 # -*- encoding: utf-8 -*-
 """
-ElasticNetH2O solver tests using Kaggle datasets.
+Linear Regression solver tests using daal library.
 
 :copyright: 2017 H2O.ai, Inc.
 :license:   Apache License Version 2.0 (see LICENSE for details)
 """
-import numpy as np
-import h2o4gpu
-import logging
-from scipy import stats
-from daal.data_management import HomogenNumericTable
-from h2o4gpu.solvers.daal_solver.normalize import zscore as z_score
-from h2o4gpu.solvers.daal_solver.daal_data import getNumpyArray
-from numpy.ma.testutils import assert_array_almost_equal
+try:
+    __import__('daal')
+except ImportError:
+    import platform
+    print("Daal is not supported. Architecture detected {}".format(platform.architecture()))
+else:
+    import numpy as np
+    import h2o4gpu
+    import logging
+    from scipy import stats
+    from daal.data_management import HomogenNumericTable
+    from h2o4gpu.solvers.daal_solver.normalize import zscore as z_score
+    from h2o4gpu.solvers.daal_solver.daal_data import getNumpyArray
+    from numpy.ma.testutils import assert_array_almost_equal
 
-logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
-def test_zscore_single():
+    def test_zscore_single():
 
-    input = np.random.rand(10,1)
-    sc_zscore = stats.zscore(input, axis=0, ddof=1)
-    
-    da_input = HomogenNumericTable(input)
-    da_zscore = z_score(da_input)
-    np_da_zscore = getNumpyArray(da_zscore)
-    
-    assert_array_almost_equal(sc_zscore, np_da_zscore)
+        input = np.random.rand(10,1)
+        sc_zscore = stats.zscore(input, axis=0, ddof=1)
 
-def test_zscore_multicolumns():
+        da_input = HomogenNumericTable(input)
+        da_zscore = z_score(da_input)
+        np_da_zscore = getNumpyArray(da_zscore)
 
-    input = np.random.rand(10,3)
-    sc_zscore = stats.zscore(input, axis=0, ddof=1)
-    
-    da_input = HomogenNumericTable(input)
-    da_zscore = z_score(da_input)
-    np_da_zscore = getNumpyArray(da_zscore)
-    
-    assert_array_almost_equal(sc_zscore, np_da_zscore)
+        assert_array_almost_equal(sc_zscore, np_da_zscore)
 
-def test_zscore_vector(): test_zscore_single()
-def test_zscore_matrix(): test_zscore_multicolumns()
+    def test_zscore_multicolumns():
 
-if __name__ == '__main__':
-    test_zscore()
+        input = np.random.rand(10,3)
+        sc_zscore = stats.zscore(input, axis=0, ddof=1)
+
+        da_input = HomogenNumericTable(input)
+        da_zscore = z_score(da_input)
+        np_da_zscore = getNumpyArray(da_zscore)
+
+        assert_array_almost_equal(sc_zscore, np_da_zscore)
+
+    def test_zscore_vector(): test_zscore_single()
+    def test_zscore_matrix(): test_zscore_multicolumns()
+
+    if __name__ == '__main__':
+        test_zscore()
