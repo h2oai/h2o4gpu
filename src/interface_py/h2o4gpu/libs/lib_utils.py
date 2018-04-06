2bf6d33cb519df6d54cb8f6530574ad42622ae76
mdymczyk <dymczyk@gmail.com>
2018-04-06 20:26:27 +0900

    Suppress SWIG generated deprecation warning

--- src/interface_py/h2o4gpu/libs/lib_utils.py
+++ src/interface_py/h2o4gpu/libs/lib_utils.py
@@ -2,7 +2,7 @@
 :copyright: 2017 H2O.ai, Inc.
 :license:   Apache License Version 2.0 (see LICENSE for details)
 """
-
+import warnings
 
 class CPUlib(object):
     """H2O4GPU CPU module"""
@@ -13,15 +13,18 @@ class CPUlib(object):
     @staticmethod
     def get():
         """Get the CPU module object"""
-        try:
-            import h2o4gpu.libs.ch2o4gpu_cpu as ch2o4gpu_cpu
-            return ch2o4gpu_cpu
-        except ImportError as e:
-            print("Exception:")
-            print(e)
-            print('\nWarning: h2o4gpu shared object (dynamic library)'
-                  ' for CPU failed to load.')
-            return None
+        # SWIG generated files contain some deprecated calls to imp
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            try:
+                import h2o4gpu.libs.ch2o4gpu_cpu as ch2o4gpu_cpu
+                return ch2o4gpu_cpu
+            except ImportError as e:
+                print("Exception:")
+                print(e)
+                print('\nWarning: h2o4gpu shared object (dynamic library)'
+                      ' for CPU failed to load.')
+                return None
 
 
 class GPUlib(object):
@@ -33,15 +36,18 @@ class GPUlib(object):
     @staticmethod
     def get():
         """Get the GPU module object"""
-        try:
-            import h2o4gpu.libs.ch2o4gpu_gpu as ch2o4gpu_gpu
-            return ch2o4gpu_gpu
-        except ImportError as e:
-            print("Exception:")
-            print(e)
-            print('\nWarning: h2o4gpu shared object (dynamic library)'
-                  ' for GPU failed to load.')
-            return None
+        # SWIG generated files contain some deprecated calls to imp
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            try:
+                import h2o4gpu.libs.ch2o4gpu_gpu as ch2o4gpu_gpu
+                return ch2o4gpu_gpu
+            except ImportError as e:
+                print("Exception:")
+                print(e)
+                print('\nWarning: h2o4gpu shared object (dynamic library)'
+                      ' for GPU failed to load.')
+                return None
 
 def get_lib(n_gpus, devices, verbose=0):
     """Load either CPU or GPU H2O4GPU library."""
