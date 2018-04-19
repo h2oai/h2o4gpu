b93119960966d658f4ff792f887d2809b572c63e
navdeep-G <mr.navdeepgill@gmail.com>
2018-04-19 11:31:36 -0700

    Modify prep_data() function. Ensure data.table is imported when h2o4gpu is loaded.

--- src/interface_r/R/data_prep.R
+++ src/interface_r/R/data_prep.R
@@ -1,18 +1,43 @@
-#'Prep data for h2o4gpu
+#'Prep data for h2o4gpu algorithms
 #'
-#'@param data_path Path to data that needs to be preprocessed for h2o4gpu
-#'@param response Response column as a string
-#'@param output_csv_name Name of saved csv file
+#'@param data_table `data.table` object containing data that needs to be preprocessed for h2o4gpu
+#'@param data_path Path to data that needs to be preprocessed for h2o4gpu. Only needed if `data_table` parameter is not provided/available
+#'@param response Response column as a string or index
+#'@param save_as_cv Whether to save processed data as a csv
+#'@param output_csv_path Path to save processed data as a csv
 #'@export
-prep_data <- function(data_path="", response="", output_csv_name="data.csv"){
-  wd <- getwd()
-  print(paste0("CSV will be saved to current working directory -> ",wd))
+prep_data <- function(data_table=NULL, data_path=NULL, response=NULL, save_as_csv=FALSE, save_csv_path=NULL){
   
-  response <- response
-  print(paste0("Response is -> ",response))
+  if (!is.null(save_as_csv)) {
+    if (!is.null(save_csv_path)) {
+      print(paste0("CSV will be saved to ", save_csv_path))
+    } else {
+      save_csv_path <- getwd()
+      print(paste0("CSV will be saved to current working directory -> ",save_csv_path, " since `save_csv_path` was not specified"))
+    }
+  }
   
-  print(paste0("Reading in -> ",data_path))
-  DT <- fread(data_path)
+  if (is.null(data_table)) {
+    if (!is.null(data_path)) {
+      print(paste0("Reading in -> ",data_path))
+      DT <- fread(data_path)
+    } else {
+      stop("Parameters `data_table` & `data_path` are NULL. Need to specify at least one.")
+    }
+  } else {
+    DT <- data_table
+  }
+  
+  if (!is.null(response)) {
+    response <- response
+    if (is.character(response)) {
+      print(paste0("Response is -> ",response))
+    } else {
+      print(paste0("Response is -> ",colnames(DT)[response]))
+    }
+  } else {
+    stop("Response is not specified")
+  }
   
   print("Number of columns:")
   print(ncol(DT))
@@ -76,6 +101,10 @@ prep_data <- function(data_path="", response="", output_csv_name="data.csv"){
   all(sapply(DT, function(x) all(is.finite(x))))
   
   ## save preprocessed file as CSV
-  print(paste0("Saving processed data to ", wd))
-  fwrite(DT, output_csv_name)
+  if (save_as_csv) {
+    print(paste0("Saving processed data to ", save_csv_path))
+    fwrite(DT, output_csv_name)
+  }
+  
+  return(DT)
 }
--- src/interface_r/R/imports.R
+++ src/interface_r/R/imports.R
@@ -1,4 +1,5 @@
 #' @importFrom magrittr %>%
 #' @importFrom reticulate use_condaenv use_python use_virtualenv r_to_py
 #' @importFrom utils packageVersion
+#' @import data.table
 NULL
