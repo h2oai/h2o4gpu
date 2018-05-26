7cf17e991541e44cc950dbd5bbffe45f9655cf28
mdymczyk <dymczyk@gmail.com>
2018-05-26 14:52:49 +0900

    Fail when NVML not found, look also for stubs

--- make/config.mk
+++ make/config.mk
@@ -41,5 +41,14 @@ R_VERSION = 3.1.0
 
 #
 # Find NVML library
-NVML_LIB := $(shell find /lib/ /usr/|grep libnvidia-ml.so.1|head -1)
+
+# Certain dockers have only stubs under: /usr/local/cuda-X.Y/targets/ARCH/lib/stubs/libnvidia-ml.so
+# Certain OS's have it under /lib, /lib64, /usr/lib or /usr/lib64
+NVML_LIB := $(shell find /lib/ /lib64 /usr/lib /usr/lib64 /usr/local/ | grep libnvidia-ml.so | sort -n | head -1)
+
+# Fail if not found - the build will pass but will fail during runtime
+ifndef NVML_LIB
+$(error NVML_LIB couldn't be found on your system. Please make sure libnvidia-ml.so is in /lib or /usr (os subdirectories))
+endif
+
 $(warning Compiling with NVML_LIB=$(NVML_LIB))
