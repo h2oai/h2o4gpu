d30e53f12ca98a9dfbe571fb28e51e6cf78a5b68
Hemen Kapadia <hemen.kapadia@gmail.com>
2018-08-09 14:10:05 -0700

    Update docker build process to generate conda package

--- scripts/make-docker-devel.sh
+++ scripts/make-docker-devel.sh
@@ -20,12 +20,18 @@ $DOCKER_CLI exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
 echo "shallow clone, and make buildinstall with ${H2O4GPU_BUILD} and ${H2O4GPU_SUFFIX}"
 $DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo ; make ${makeopts} buildinstall H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
 
+echo "Docker devel - Creating conda package"
+$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo/conda-recipe; conda build purge && conda build -c h2oai -c conda-forge ."
+
 echo "Docker devel - Clean local wheels and Copying wheel from docker"
 rm -rf src/interface_py/dist/
 $DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/src/interface_py/dist src/interface_py/
 
 echo "Docker devel - Copying VERSION.txt"
 mkdir -p build ; $DOCKER_CLI cp ${CONTAINER_NAME}:/root/repo/build/VERSION.txt build/
+
+echp "Docker devel - Copying conda package"
+[ -d condapkg ] && rm -rf condapkg; $DOCKER_CLI cp -a ${CONTAINER_NAME}:/opt/h2oai/h2o4gpu/python/conda condapkg
 
 echo "Docker devel - Stopping docker"
 $DOCKER_CLI stop ${CONTAINER_NAME}
