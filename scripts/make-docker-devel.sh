6a1d3b15022368840b7d931302b196219ce69d39
Hemen Kapadia <hemen.kapadia@gmail.com>
2018-08-14 14:07:07 -0700

    Output folder specified for conda-build

--- scripts/make-docker-devel.sh
+++ scripts/make-docker-devel.sh
@@ -21,7 +21,8 @@ echo "shallow clone, and make buildinstall with ${H2O4GPU_BUILD} and ${H2O4GPU_S
 $DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo ; make ${makeopts} buildinstall H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
 
 echo "Docker devel - Creating conda package"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo/conda-recipe; conda build purge && conda build -c h2oai -c conda-forge ."
+$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "mkdir -p repo/condapkgs"
+$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "pushd repo/conda-recipe; conda build --output-folder ../condapkgs  -c h2oai -c conda-forge .; popd"
 
 echo "Docker devel - Clean local wheels and Copying wheel from docker"
 rm -rf src/interface_py/dist/
@@ -30,8 +31,9 @@ $DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/src/interface_py/dist src/interfa
 echo "Docker devel - Copying VERSION.txt"
 mkdir -p build ; $DOCKER_CLI cp ${CONTAINER_NAME}:/root/repo/build/VERSION.txt build/
 
-echp "Docker devel - Copying conda package"
-[ -d condapkg ] && rm -rf condapkg; $DOCKER_CLI cp -a ${CONTAINER_NAME}:/opt/h2oai/h2o4gpu/python/conda condapkg
+echo "Docker devel - Copying conda package"
+rm -rf condapkgs
+$DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/condapkgs .
 
 echo "Docker devel - Stopping docker"
 $DOCKER_CLI stop ${CONTAINER_NAME}
