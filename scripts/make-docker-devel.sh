97dfb2ff69764ac57b0f2f86f1992a5fd7d1e30b
Hemen Kapadia <hemen.kapadia@gmail.com>
2018-09-07 01:00:12 +0000

    Changes to build docker with llvm6

--- scripts/make-docker-devel.sh
+++ scripts/make-docker-devel.sh
@@ -12,34 +12,34 @@ CONDA_PKG_NAME="h2o4gpu${extratag}"
 
 #--build-arg http_proxy=http://172.16.2.142:3128/
 echo "Docker devel - BEGIN"
-$DOCKER_CLI build -t opsh2oai/h2o4gpu-buildversion${extratag}-build -f Dockerfile-build --rm=false --build-arg docker_name=${dockerimage} .
+$DOCKER_CLI build -t opsh2oai/h2o4gpu-buildversion${extratag}-build-llvm6 -f Dockerfile-build --rm=false --build-arg docker_name=${dockerimage} .
 
-$DOCKER_CLI run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
+#$DOCKER_CLI run --init --rm --name ${CONTAINER_NAME} -d -t -u root -v `pwd`:/dot  --entrypoint=bash opsh2oai/h2o4gpu-buildversion${extratag}-build
 
 echo "Docker devel - Copying files"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c 'mkdir -p repo ; cp -a /dot/. ./repo'
 
 echo "shallow clone, and make buildinstall with ${H2O4GPU_BUILD} and ${H2O4GPU_SUFFIX}"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo ; make ${makeopts} buildinstall H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo ; make ${makeopts} buildinstall H2O4GPU_BUILD=${H2O4GPU_BUILD} H2O4GPU_SUFFIX=${H2O4GPU_SUFFIX}"
 
 echo "Docker devel - Creating conda package"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "mkdir -p repo/condapkgs"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo/src/interface_py; cat requirements_*.txt | grep -v '#' | sort | uniq > requirements_conda.txt"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "pushd repo/conda-recipe; sed -i 's/condapkgname/${CONDA_PKG_NAME}/g' meta.yaml; popd"
-$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "pushd repo/conda-recipe; conda build --output-folder ../condapkgs  -c h2oai -c conda-forge .; popd"
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "mkdir -p repo/condapkgs"
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "cd repo/src/interface_py; cat requirements_*.txt | grep -v '#' | sort | uniq > requirements_conda.txt"
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "pushd repo/conda-recipe; sed -i 's/condapkgname/${CONDA_PKG_NAME}/g' meta.yaml; popd"
+#$DOCKER_CLI exec ${CONTAINER_NAME} bash -c "pushd repo/conda-recipe; conda build --output-folder ../condapkgs  -c h2oai -c conda-forge .; popd"
 
 echo "Docker devel - Clean local wheels and Copying wheel from docker"
-rm -rf src/interface_py/dist/
-$DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/src/interface_py/dist src/interface_py/
+#rm -rf src/interface_py/dist/
+#$DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/src/interface_py/dist src/interface_py/
 
 echo "Docker devel - Copying VERSION.txt"
-mkdir -p build ; $DOCKER_CLI cp ${CONTAINER_NAME}:/root/repo/build/VERSION.txt build/
+#mkdir -p build ; $DOCKER_CLI cp ${CONTAINER_NAME}:/root/repo/build/VERSION.txt build/
 
 echo "Docker devel - Copying conda package"
-rm -rf condapkgs
-$DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/condapkgs .
+#rm -rf condapkgs
+#$DOCKER_CLI cp -a ${CONTAINER_NAME}:/root/repo/condapkgs .
 
 echo "Docker devel - Stopping docker"
-$DOCKER_CLI stop ${CONTAINER_NAME}
+#$DOCKER_CLI stop ${CONTAINER_NAME}
 
 echo "Docker devel - END"
