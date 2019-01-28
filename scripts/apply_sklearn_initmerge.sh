e000526b81f2d27a9f65bd155eecb00067681104
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-01-28 18:17:53 +0100

    Embed build parameters as package's variables

--- scripts/apply_sklearn_initmerge.sh
+++ scripts/apply_sklearn_initmerge.sh
@@ -4,5 +4,5 @@
 rm -rf src/interface_py/h2o4gpu/__init__.py
 cat sklearn/h2o4gpu/__init__.py | sed 's/__version__.*//g' >> src/interface_py/h2o4gpu/__init__.py.2
 
-cat src/interface_py/h2o4gpu/__init__.base.py src/interface_py/h2o4gpu/__init__.py.2 > src/interface_py/h2o4gpu/__init__.py
+cat src/interface_py/h2o4gpu/__init__.base.py src/interface_py/build_info.txt src/interface_py/h2o4gpu/__init__.py.2 > src/interface_py/h2o4gpu/__init__.py
 rm -rf src/interface_py/h2o4gpu/__init__.py.2
