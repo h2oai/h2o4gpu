29f55baabcb82b59d759b832d5cdabbdb58845f6
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-03-19 20:19:45 +0100

    add stub version back

--- scripts/apply_sklearn_initmerge.sh
+++ scripts/apply_sklearn_initmerge.sh
@@ -9,6 +9,8 @@ cat >> src/interface_py/h2o4gpu/__init__.py <<EOL
 :copyright: 2017-2019 H2O.ai, Inc.
 :license:   Apache License Version 2.0 (see LICENSE for details)
 """
+# stub value	# Skip pylint b / c this is automatically concatenated at compile time
+__version__ = "0.0.0.0000"
 EOL
 
 cat src/interface_py/build_info.txt src/interface_py/h2o4gpu/__init__.py.2  src/interface_py/h2o4gpu/__init__.base.py >> src/interface_py/h2o4gpu/__init__.py
