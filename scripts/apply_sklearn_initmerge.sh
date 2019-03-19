775b3a99f5ca791865083f4e96b206d0d7177b8e
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-03-19 15:27:31 +0100

    * use cython==0.29.3 everywhere * fix for __init__.py

--- scripts/apply_sklearn_initmerge.sh
+++ scripts/apply_sklearn_initmerge.sh
@@ -4,5 +4,12 @@
 rm -rf src/interface_py/h2o4gpu/__init__.py
 cat sklearn/h2o4gpu/__init__.py | sed 's/__version__.*//g' >> src/interface_py/h2o4gpu/__init__.py.2
 
-cat src/interface_py/h2o4gpu/__init__.base.py src/interface_py/build_info.txt src/interface_py/h2o4gpu/__init__.py.2 > src/interface_py/h2o4gpu/__init__.py
+cat >> src/interface_py/h2o4gpu/__init__.py <<EOL
+"""
+:copyright: 2017-2019 H2O.ai, Inc.
+:license:   Apache License Version 2.0 (see LICENSE for details)
+"""
+EOL
+
+cat src/interface_py/build_info.txt src/interface_py/h2o4gpu/__init__.py.2  src/interface_py/h2o4gpu/__init__.base.py >> src/interface_py/h2o4gpu/__init__.py
 rm -rf src/interface_py/h2o4gpu/__init__.py.2
--- src/interface_py/h2o4gpu/__init__.base.py
+++ src/interface_py/h2o4gpu/__init__.base.py
@@ -1,43 +1,37 @@
 # pylint: skip-file
-"""
-:copyright: 2017-2018 H2O.ai, Inc.
-:license:   Apache License Version 2.0 (see LICENSE for details)
-"""
-#Skip pylint b / c this is automatically concatenated at compile time
-#with other init files
+from .util import import_data
+from .util import metrics
+from . import h2o4gpu_exceptions
+from .typecheck import compatibility
+from .typecheck import typechecks
+from .solvers.truncated_svd import TruncatedSVDH2O
+from .solvers.truncated_svd import TruncatedSVD
+from .solvers.pca import PCAH2O
+from .solvers.pca import PCA
+from .solvers.kmeans import KMeansH2O
+from .solvers.kmeans import KMeans
+from .solvers.xgboost import GradientBoostingRegressor
+from .solvers.xgboost import GradientBoostingClassifier
+from .solvers.xgboost import RandomForestClassifier
+from .solvers.xgboost import RandomForestRegressor
+from .solvers.ridge import Ridge
+from .solvers.lasso import Lasso
+from .solvers.linear_regression import LinearRegression
+from .solvers.logistic import LogisticRegression
+from .solvers.elastic_net import ElasticNetH2O
+from .solvers.elastic_net import ElasticNet
+from .solvers.pogs import Pogs
+from .types import FunctionVector
 
-# stub value
-__version__ = "0.0.0.0000"
+# Skip pylint b / c this is automatically concatenated at compile time
+# with other init files
 
-DAAL_SUPPORTED=True
+DAAL_SUPPORTED = True
 
 try:
     __import__('daal')
 except ImportError:
-    DAAL_SUPPORTED=False
+    DAAL_SUPPORTED = False
 
 if DAAL_SUPPORTED:
     from .solvers.daal_solver.regression import Method as LinearMethod
-from .types import FunctionVector
-from .solvers.pogs import Pogs
-from .solvers.elastic_net import ElasticNet
-from .solvers.elastic_net import ElasticNetH2O
-from .solvers.logistic import LogisticRegression
-from .solvers.linear_regression import LinearRegression
-from .solvers.lasso import Lasso
-from .solvers.ridge import Ridge
-from .solvers.xgboost import RandomForestRegressor
-from .solvers.xgboost import RandomForestClassifier
-from .solvers.xgboost import GradientBoostingClassifier
-from .solvers.xgboost import GradientBoostingRegressor
-from .solvers.kmeans import KMeans
-from .solvers.kmeans import KMeansH2O
-from .solvers.pca import PCA
-from .solvers.pca import PCAH2O
-from .solvers.truncated_svd import TruncatedSVD
-from .solvers.truncated_svd import TruncatedSVDH2O
-from .typecheck import typechecks
-from .typecheck import compatibility
-from . import h2o4gpu_exceptions
-from .util import metrics
-from .util import import_data
