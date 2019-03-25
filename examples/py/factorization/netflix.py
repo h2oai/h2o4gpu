06797b1cd0bddeb5d939107de52e4bff67fd929e
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-03-25 15:16:56 +0100

    Update netflix.py

--- examples/py/factorization/netflix.py
+++ examples/py/factorization/netflix.py
@@ -3,7 +3,6 @@ import numpy as np
 import h2o4gpu
 from sklearn.decomposition import NMF
 from sklearn.metrics import mean_squared_error
-import argparse
 
 _lib_h2o4gpu = 'h2o4gpu'
 _lib_sklearn = 'sklearn'
@@ -18,31 +17,13 @@ if __name__ == '__main__':
     train = scipy.sparse.load_npz('../data/netflix_train.npz').tocoo()
     test = scipy.sparse.load_npz('../data/netflix_test.npz').tocoo()
 
-    n_components = 20
+    n_components = 10
 
     if args.lib == _lib_h2o4gpu:
         scores = []
         factorization = h2o4gpu.solvers.FactorizationH2O(
             n_components, 0.005, max_iter=85)
         factorization.fit(train, X_test=test, scores=scores, verbose=True)
-        user_vec = np.take(factorization.XT, train.row, axis=0)
-        movie_vec = np.take(factorization.thetaT, train.col, axis=0)
-        features = np.concatenate([user_vec, movie_vec], axis=1)
-        print('training boosting model')
-        boosting = h2o4gpu.GradientBoostingRegressor(
-            verbose=True, criterion='friedman_mse')
-        boosting.fit(features, y=train.data)
-        pred = boosting.predict(features)
-        print('train score: {0}'.format(
-            np.sqrt(mean_squared_error(pred, train.data))))
-
-        user_vec = np.take(factorization.XT, test.row, axis=0)
-        movie_vec = np.take(factorization.thetaT, test.col, axis=0)
-        features = np.stack([user_vec, movie_vec], axis=1)
-        pred = boosting.predict(features, y=test.data)
-        print('cv score: {0}'.format(
-            np.sqrt(mean_squared_error(pred, train.data))))
-
     else:
         model = NMF(n_components=n_components, init='random',
                     random_state=0, max_iter=100)
