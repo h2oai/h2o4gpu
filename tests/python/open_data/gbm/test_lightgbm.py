6047af6c22982c46377deb974ff93d51e9f6c8ab
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-03-31 23:48:41 +0200

    Update Makefile and test_lightgbm.py

--- tests/python/open_data/gbm/test_lightgbm.py
+++ tests/python/open_data/gbm/test_lightgbm.py
@@ -74,6 +74,8 @@ def test_lightgbm_cpu(booster):
                 stratified=False,
                 verbose_eval=50)
 
+
+@pytest.mark.skip(reason="FIXME")
 @pytest.mark.parametrize('booster,', ["dart", "gbdt"])
 def test_lightgbm_cpu_airlines_full(booster):
     import numpy as np
@@ -122,11 +124,12 @@ def test_lightgbm_cpu_airlines_full(booster):
                 stratified=False,
                 verbose_eval=10)
 
-@pytest.mark.skip(reason="FIXME")
+
 @pytest.mark.parametrize('booster,', ["dart", "gbdt"])
 @pytest.mark.parametrize('year,', ["1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994",
                                    "1995", "1996", "1997", "1998", "1999", "2000", "2001",
-                                   "2002", "2003", "2004", "2005", "2006", "2007"])
+                                   "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009",
+                                   "2010", "2011", "2012", "2013"])
 def test_lightgbm_cpu_airlines_year(booster, year):
     import numpy as np
     import pandas as pd
