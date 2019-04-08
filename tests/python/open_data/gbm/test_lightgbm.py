0ef52ed70c7633f76f08f7b4843551eec962384f
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-04-08 23:57:45 +0200

    add gpu tests

--- tests/python/open_data/gbm/test_lightgbm.py
+++ tests/python/open_data/gbm/test_lightgbm.py
@@ -179,6 +179,63 @@ def test_lightgbm_cpu_airlines_year(booster, year):
                 verbose_eval=10)
 
 
+@pytest.mark.skipif(platform.machine().startswith("ppc64le"), reason="lightgbm on gpu is not supported yet")
+@pytest.mark.parametrize('booster,', ["dart", "gbdt"])
+@pytest.mark.parametrize('year,', ["1987",
+                                   # "1988", "1989", "1990", "1991", "1992", "1993", "1994",
+                                   #                                    "1995", "1996", "1997", "1998", "1999", "2000", "2001",
+                                   #                                    "2002", "2003", "2004", "2005", "2006", "2007"
+                                   ])
+def test_lightgbm_gpu_airlines_year(booster, year):
+    import numpy as np
+    import pandas as pd
+    from h2o4gpu.util.lightgbm_dynamic import got_cpu_lgb, got_gpu_lgb
+    import lightgbm as lgb
+
+    data = pd.read_csv('./open_data/airlines/year{0}.zip'.format(year),
+                       dtype={'UniqueCarrier': 'category', 'Origin': 'category', 'Dest': 'category',
+                              'TailNum': 'category', 'CancellationCode': 'category',
+                              'IsArrDelayed': 'category', 'IsDepDelayed': 'category',
+                              'DepTime': np.float32, 'CRSDepTime': np.float32, 'ArrTime': np.float32,
+                              'CRSArrTime': np.float32, 'ActualElapsedTime': np.float32,
+                              'CRSElapsedTime': np.float32, 'AirTime': np.float32,
+                              'ArrDelay': np.float32, 'DepDelay': np.float32, 'Distance': np.float32,
+                              'TaxiIn': np.float32, 'TaxiOut': np.float32, 'Diverted': np.float32,
+                              'Year': np.int32, 'Month': np.int32, 'DayOfWeek': np.int32,
+                              'DayofMonth': np.int32, 'Cancelled': 'category',
+                              'CarrierDelay': np.float32, 'WeatherDelay': np.float32,
+                              'NASDelay': np.float32, 'SecurityDelay': np.float32,
+                              'LateAircraftDelay': np.float32})
+
+    y = data["IsArrDelayed"].cat.codes
+    data = data[['UniqueCarrier', 'Origin', 'Dest', 'IsDepDelayed', 'Year', 'Month',
+                 'DayofMonth', 'DayOfWeek', 'DepTime', 'CRSDepTime',
+                 'ArrTime', 'CRSArrTime', 'FlightNum', 'TailNum',
+                 'ActualElapsedTime', 'CRSElapsedTime', 'AirTime', 'ArrDelay',
+                 'DepDelay', 'Distance', 'TaxiIn', 'TaxiOut',
+                 'Cancelled', 'CancellationCode', 'Diverted', 'CarrierDelay',
+                 'WeatherDelay', 'NASDelay', 'SecurityDelay', 'LateAircraftDelay']]
+
+    lgb_params = {'learning_rate': 0.1,
+                  'boosting': booster,
+                  'objective': 'binary',
+                  'metric': 'rmse',
+                  'feature_fraction': 0.9,
+                  'bagging_fraction': 0.75,
+                  'num_leaves': 31,
+                  'bagging_freq': 1,
+                  'min_data_per_leaf': 250,
+                  'device_type': 'gpu',
+                  'gpu_device_id': 0}
+    lgb_train = lgb.Dataset(data=data, label=y)
+    cv = lgb.cv(lgb_params,
+                lgb_train,
+                num_boost_round=50,
+                early_stopping_rounds=5,
+                stratified=False,
+                verbose_eval=10)
+
+
 if __name__ == '__main__':
     pass
     # test_lightgbm_cpu()
