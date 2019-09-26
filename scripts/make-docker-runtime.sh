83fe9cb17b5b25d4bc3e625ca76918f4efec787f
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-09-26 23:54:45 +0200

    fix docker build-arg

--- scripts/make-docker-runtime.sh
+++ scripts/make-docker-runtime.sh
@@ -10,7 +10,7 @@ echo "Docker runtime - BEGIN"
 echo "Docker runtime - Build"
 # wheel=${encodedFullVersionTag}${extratag}/h2o4gpu-${encodedFullVersionTag}-cp36-cp36m-linux_x86_64.whl # use this if want to pull from s3 in Dockerfile-runtime
 #  --build-arg http_proxy=http://172.16.2.142:3128/
-$DOCKER_CLI build -t opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime:latest -f Dockerfile-runtime --rm=false --build-arg docker_name=${dockerimage} use_miniconda=1 .
+$DOCKER_CLI build -t opsh2oai/h2o4gpu-${versionTag}${extratag}-runtime:latest -f Dockerfile-runtime --rm=false --build-arg docker_name=${dockerimage} --build-arg use_miniconda=1 .
 # -u `id -u`:`id -g` -d -t -w `pwd` -v `pwd`:`pwd`:rw
 
 echo "Runtime Docker - Run"
