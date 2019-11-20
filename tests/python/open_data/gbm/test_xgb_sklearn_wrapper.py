f745d355b9ddf673f364308189f579e23f245a00
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2019-11-20 15:12:10 +0100

    reproduce #806

--- tests/python/open_data/gbm/test_xgb_sklearn_wrapper.py
+++ tests/python/open_data/gbm/test_xgb_sklearn_wrapper.py
@@ -254,6 +254,43 @@ def test_gbm_classifier_backupsklearn(backend='auto'):
         print(gbm_sk.train_score_)
         assert (gbm.train_score_ == gbm_sk.train_score_).all() == True
 
+def gbm_regressor_parameter_fallback(**kwargs):
+    df = pd.read_csv("./open_data/simple.txt", delim_whitespace=True)
+    X = np.array(df.iloc[:, :df.shape[1] - 1], dtype='float32', order='C')
+    y = np.array(df.iloc[:, df.shape[1] - 1], dtype='float32', order='C')
+    import h2o4gpu
+
+    gbm = h2o4gpu.GradientBoostingRegressor(**kwargs)
+    print("h2o4gpu fit()")
+    gbm.fit(X, y)
+
+    from h2o4gpu.ensemble import GradientBoostingRegressorSklearn
+    gbm_sk = GradientBoostingRegressorSklearn(**kwargs)
+    print("Scikit fit()")
+    gbm_sk.fit(X, y)
+
+    assert np.allclose(gbm.predict(X), gbm_sk.predict(X))
+
+def test_gbm_regressor_parameter_loss_alpha_issue_806():
+    df = pd.read_csv("./open_data/simple.txt", delim_whitespace=True)
+    X = np.array(df.iloc[:, :df.shape[1] - 1], dtype='float32', order='C')
+    y = np.array(df.iloc[:, df.shape[1] - 1], dtype='float32', order='C')
+    import h2o4gpu
+
+    print(X.shape, y.shape)
+
+    Solver = h2o4gpu.GradientBoostingRegressor
+
+    gbm = h2o4gpu.GradientBoostingRegressor(loss='ls', alpha=0.9)
+
+    gbm.fit(X, y)
+
+    gbm_sk = h2o4gpu.GradientBoostingRegressor(loss='quantile', alpha=0.9)
+
+    gbm_sk.fit(X, y)
+
+    assert not np.allclose(gbm.predict(X), gbm_sk.predict(X))
+
 
 def test_sklearn_drf_regression(): test_drf_regressor_backupsklearn()
 
@@ -297,3 +334,10 @@ def test_sklearn_gbm_regression_sklearn(
 
 def test_sklearn_gbm_regression_h2o4gpu(
 ): test_gbm_regressor_backupsklearn(backend='h2o4gpu')
+
+def test_gbm_regressor_parameter_fallback_loss() : gbm_regressor_parameter_fallback(loss='quantile')
+
+def test_gbm_regressor_parameter_fallback_loss_alpha() : gbm_regressor_parameter_fallback(alpha=0.9, loss='quantile')
+
+if __name__ == '__main__':
+    test_gbm_regressor_parameter_loss_alpha()
