67fa34edfadf4333bea07e94656ca46dadcde6da
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2020-01-31 20:58:46 +0100

    revert multi-gpu tests

--- tests/python/open_data/gbm/test_xgboost.py
+++ tests/python/open_data/gbm/test_xgboost.py
@@ -17,12 +17,7 @@ print(sys.path)
 logging.basicConfig(level=logging.DEBUG)
 
 
-def setup_module(module):
-    from sklearn.datasets import fetch_covtype
-    _ = fetch_covtype()
-
-
-@pytest.mark.parametrize("n_gpus", [0, 1])
+@pytest.mark.parametrize("n_gpus", [-1, 1, None])
 def test_xgboost_covtype(n_gpus):
     import xgboost as xgb
     import numpy as np
@@ -71,90 +66,3 @@ def test_xgboost_covtype(n_gpus):
     xgb.train(param, dtrain, num_round, evals=[
               (dtest, 'test')], evals_result=cpu_res)
     print("CPU Training Time: %s seconds" % (str(time.time() - tmp)))
-
-
-@pytest.mark.multi_gpu
-def test_xgboost_covtype_multi_gpu():
-    import xgboost as xgb
-    import numpy as np
-    from sklearn.datasets import fetch_covtype
-    from sklearn.model_selection import train_test_split
-    import time
-    from dask_cuda import LocalCUDACluster
-    from dask.distributed import Client
-    from dask import array as da
-    import xgboost as xgb
-    from xgboost.dask import DaskDMatrix
-    from dask import array as da
-
-    # Fetch dataset using sklearn
-    cov = fetch_covtype()
-    X = cov.data
-    y = cov.target
-
-    print(X.shape, y.shape)
-
-    # Create 0.75/0.25 train/test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, train_size=0.75,
-                                                        random_state=42)
-
-    # Specify sufficient boosting iterations to reach a minimum
-    num_round = 10
-
-    # Leave most parameters as default
-    param = {'objective': 'multi:softmax',  # Specify multiclass classification
-             'num_class': 8,  # Number of possible output classes
-             'tree_method': 'gpu_hist',  # Use GPU accelerated algorithm
-             }
-
-    from h2o4gpu.util.gpu import device_count
-    n_gpus, devices = device_count(-1)
-
-    with LocalCUDACluster(n_workers=n_gpus, threads_per_worker=1) as cluster:
-        with Client(cluster) as client:
-            # Convert input data from numpy to XGBoost format
-            partition_size = 100000
-
-            # remove when https://github.com/dmlc/xgboost/issues/4987 is fixed
-            dask_X_train = da.from_array(X_train, partition_size)
-            dask_X_train = dask_X_train.persist()
-            client.rebalance(dask_X_train)
-            dask_label_train = da.from_array(y_train, partition_size)
-            dask_label_train = dask_label_train.persist()
-            client.rebalance(dask_label_train)
-
-            dtrain = DaskDMatrix(
-                client=client, data=dask_X_train, label=dask_label_train)
-
-            dask_X_test = da.from_array(X_test, partition_size)
-            dask_X_test = dask_X_test.persist()
-            client.rebalance(dask_X_test)
-            dask_label_test = da.from_array(y_test, partition_size)
-            dask_label_test = dask_label_test.persist()
-            client.rebalance(dask_label_test)
-
-            dtest = DaskDMatrix(
-                client=client, data=dask_X_test, label=dask_label_test)
-
-            gpu_res = {}  # Store accuracy result
-            tmp = time.time()
-            # Train model
-            xgb.dask.train(client, param, dtrain, num_boost_round=num_round, evals=[
-                (dtest, 'test')])
-            print("GPU Training Time: %s seconds" % (str(time.time() - tmp)))
-
-            # TODO: https://github.com/dmlc/xgboost/issues/4518
-            dtrain = xgb.DMatrix(X_train, label=y_train, nthread=-1)
-            dtest = xgb.DMatrix(X_test, label=y_test, nthread=-1)
-            # Repeat for CPU algorithm
-            tmp = time.time()
-            param['tree_method'] = 'hist'
-            cpu_res = {}
-            xgb.train(param, dtrain, num_round, evals=[
-                (dtest, 'test')], evals_result=cpu_res)
-            print("CPU Training Time: %s seconds" % (str(time.time() - tmp)))
-
-
-if __name__ == "__main__":
-    # pass
-    test_xgboost_covtype_multi_gpu()
