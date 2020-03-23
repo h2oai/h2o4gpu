08d5aff76a7bc4eee5d0b1f5dbb5b6ce8444989d
Vladimir <vladimir.ovsyannikov@h2o.ai>
2020-03-23 12:16:56 +0100

    update numpy to 1.18.1 (#832)
    * install local builds without dependencies
    
    * use existing deps

--- scripts/apply_sklearn_pipinstall.sh
+++ scripts/apply_sklearn_pipinstall.sh
@@ -5,5 +5,5 @@ rm -rf sklearn
 cd scikit-learn
 file=`ls dist/h2o4gpu*.whl`
 
-pip install $file --upgrade --constraint ../src/interface_py/requirements_buildonly.txt --target ../sklearn/
+pip install $file --upgrade --constraint ../src/interface_py/requirements_buildonly.txt --target ../sklearn/ --upgrade-strategy only-if-needed
 cd ../
